95c96498a3beb304e1717d13060a0484ecad45a1
saboten <none@none>
2009-10-29 19:54:55 +0000

    Undo.js v3.2.2(alpha1) ・undoExec, redoExecフラグを追加 ・操作としてオブジェクトを登録する時の処理を変更 ・undo,redo関数から共通処理としてpostproc関数を分離 ・MenuExecクラスのexpand/reduce/dlip/turn系の変更に対応

--- src/pzprBase/Undo.js
+++ src/pzprBase/Undo.js
@@ -1,4 +1,4 @@
-// Undo.js v3.2.0p1
+// Undo.js v3.2.2
 
 //---------------------------------------------------------------------------
 // ★UndoManagerクラス 操作情報を扱い、Undo/Redoの動作を実装する
@@ -12,21 +12,23 @@ Operation = function(obj, property, id, old, num){
 	this.old = old;
 	this.num = num;
 	this.chain = um.chainflag;
-	this.undoonly = um.undoonly;
 };
 
 // UndoManagerクラス
 UndoManager = function(){
-	this.ope = new Array();	// Operationクラスを保持する配列
+	this.ope = [];			// Operationクラスを保持する配列
 	this.current = 0;		// 現在の表示操作番号を保持する
 	this.disrec = 0;		// このクラスからの呼び出し時は1にする
-	this.chainflag = 0;
-	this.undoonly = 0;
+	this.chainflag = 0;		// 前のOperationとくっつけて、一回のUndo/Redoで変化できるようにする
+	this.disCombine = 0;	// 数字がくっついてしまうので、それを一時的に無効にするためのフラグ
+
+	this.anscount = 0;			// 補助以外の操作が行われた数を保持する(autocheck用)
+	this.changeflag = false;	// 操作が行われたらtrueにする(mv.notInputted()用)
+
+	this.undoExec = false;		// Undo中
+	this.redoExec = false;		// Redo中
+	this.reqReset = false;		// Undo/Redo時に盤面回転等が入っていた時、resize,resetInfo関数のcallを要求する
 	this.range = { x1:k.qcols+1, y1:k.qrows+1, x2:-2, y2:-2};
-	this.reqReset = 0;
-	this.disCombine = 0;
-	this.anscount = 0;
-	this.changeflag = false;
 };
 UndoManager.prototype = {
 	//---------------------------------------------------------------------------
@@ -37,9 +39,15 @@ UndoManager.prototype = {
 	// um.allerase()       記憶していた操作を全て破棄する
 	// um.newOperation()   マウス、キー入力開始時に呼び出す
 	//---------------------------------------------------------------------------
+
+	// 今この関数でレコード禁止になるのは、UndoRedo時、URLdecode、fileopen、adjustGeneral/Special時
+	// 連動して実行しなくなるのはaddOpe()と、LineInfo/AreaInfoの中身.
+	//  -> ここで使っているUndo/RedoとaddOpe以外はbd.QuC系関数を使用しないように変更
+	//     変な制限事項がなくなるし、動作速度にもかなり効くしね
 	disableRecord : function(){ this.disrec++; },
 	enableRecord  : function(){ if(this.disrec>0){ this.disrec--;} },
 	isenableRecord : function(){ return (this.disrec==0);},
+
 	enb_btn : function(){
 		if(!this.ope.length){
 			$("#btnundo").attr("disabled","true");
@@ -66,72 +74,57 @@ UndoManager.prototype = {
 
 	//---------------------------------------------------------------------------
 	// um.addOpe() 指定された操作を追加する。id等が同じ場合は最終操作を変更する
+	// um.addObj() 指定されたオブジェクトを操作として追加する
 	//---------------------------------------------------------------------------
 	addOpe : function(obj, property, id, old, num){
 		if(!this.isenableRecord()){ return;}
 		else if(old==num){ return;}
 
-		if(obj==property){
-			if(obj=='cell' || obj=='excell'){
-				this.addOpe(obj, 'ques', id, old.ques, 0);
-				this.addOpe(obj, 'qnum', id, old.qnum, -1);
-				this.addOpe(obj, 'direc', id, old.direc, 0);
-				this.addOpe(obj, 'qans', id, old.qans, -1);
-				this.addOpe(obj, 'qsub', id, old.qsub, 0);
-				if(old.obj){ this.addOpe(obj, 'numobj', id, old.numobj, "");}
-				if(old.obj){ this.addOpe(obj, 'numobj2', id, old.numobj2, "");}
-			}
-			else if(obj=='cross'){
-				this.addOpe('cross', 'ques', id, old.ques, -1);
-				this.addOpe('cross', 'qnum', id, old.qnum, -1);
-				if(old.obj){ this.addOpe('cross', 'numobj', id, old.numobj, "");}
-			}
-			else if(obj=='border'){
-				this.addOpe('border', 'ques', id, old.ques, 0);
-				this.addOpe('border', 'qnum', id, old.ques, 0);
-				this.addOpe('border', 'qans', id, old.qans, 0);
-				this.addOpe('border', 'qsub', id, old.qsub, 0);
-				this.addOpe('border', 'line', id, old.line, 0);
-				this.addOpe('border', 'color', id, old.color, "");
-				if(old.obj){ this.addOpe('border', 'numobj', id, old.numobj, "");}
-			}
+		var lastid = this.ope.length-1;
+
+		if(this.current < this.ope.length){
+			for(var i=this.ope.length-1;i>=this.current;i--){ this.ope.pop();}
+			lastid = -1;
+		}
+
+		// 前回と同じ場所なら前回の更新のみ
+		if(lastid>=0 && this.ope[lastid].obj == obj && this.ope[lastid].property == property && this.ope[lastid].id == id && this.ope[lastid].num == old
+			&& this.disCombine==0 && ( (obj == 'cell' && ( property=='qnum' || (property=='qans' && k.isAnsNumber) )) || obj == 'cross')
+		)
+		{
+			this.ope[lastid].num = num;
 		}
 		else{
-			var lastid = this.ope.length-1;
-
-			if(this.current < this.ope.length){
-				for(var i=this.ope.length-1;i>=this.current;i--){ this.ope.pop();}
-				lastid = -1;
-			}
-			else if(this.undoonly!=1){ lastid!=-1;}
-
-			// 前回と同じ場所なら前回の更新のみ
-			if(lastid>=0 && this.ope[lastid].obj == obj && this.ope[lastid].property == property && this.ope[lastid].id == id && this.ope[lastid].num == old
-				&& this.disCombine==0 && ( (obj == 'cell' && ( property=='qnum' || (property=='qans' && k.isAnsNumber) )) || obj == 'cross')
-			)
-			{
-				this.ope[lastid].num = num;
-			}
-			else{
-				this.ope.push(new Operation(obj, property, id, old, num));
-				this.current++;
-				if(this.chainflag==0){ this.chainflag = 1;}
-			}
+			this.ope.push(new Operation(obj, property, id, old, num));
+			this.current++;
+			if(this.chainflag==0){ this.chainflag = 1;}
 		}
+
 		if(property!='qsub' && property!='color'){ this.anscount++;}
 		this.changeflag = true;
 		this.enb_btn();
+	},
+	addObj : function(type, id){
+		var old, obj;
+		if     (type=='cell')  { old = new Cell();   obj = bd.cell[id];  }
+		else if(type=='cross') { old = new Cross();  obj = bd.cross[id]; }
+		else if(type=='border'){ old = new Border(); obj = bd.border[id];}
+		else if(type=='excell'){ old = new Cell();   obj = bd.excell[id];}
+		for(var i in obj){ old[i] = obj[i];}
+		this.addOpe(type, type, id, old, null);
 	},
 
 	//---------------------------------------------------------------------------
 	// um.undo()  Undoを実行する
 	// um.redo()  Redoを実行する
+	// um.postproc() Undo/Redo実行後の処理を行う
 	// um.exec()  操作opeを反映する。undo(),redo()から内部的に呼ばれる
 	//---------------------------------------------------------------------------
 	undo : function(){
 		if(this.current==0){ return;}
-
-		this.disableRecord(); this.range = { x1:k.qcols+1, y1:k.qrows+1, x2:-2, y2:-2};
+		this.undoExec = true;
+		this.range = { x1:k.qcols+1, y1:k.qrows+1, x2:-2, y2:-2};
+		this.disableRecord();
 		while(this.current>0){
 			this.exec(this.ope[this.current-1], this.ope[this.current-1].old);
 			if(this.ope[this.current-1].property!='qsub' && this.ope[this.current-1].property!='color'){ this.anscount--;}
@@ -139,23 +132,36 @@ UndoManager.prototype = {
 
 			if(!this.ope[this.current].chain){ break;}
 		}
-		if(this.reqReset==1){ room.resetRarea(); this.reqReset=0;}
-		this.enableRecord(); pc.paint(this.range.x1, this.range.y1, this.range.x2, this.range.y2);
-		this.enb_btn();
+		this.postproc();
+		this.undoExec = false;
+		if(this.current==0){ kc.inUNDO=false;}
 	},
 	redo : function(){
 		if(this.current==this.ope.length){ return;}
-		this.disableRecord(); this.range = { x1:k.qcols+1, y1:k.qrows+1, x2:-2, y2:-2};
+		this.redoExec = true;
+		this.range = { x1:k.qcols+1, y1:k.qrows+1, x2:-2, y2:-2};
+		this.disableRecord();
 		while(this.current<this.ope.length){
-			if(this.ope[this.current].undoonly!=1){ this.exec(this.ope[this.current], this.ope[this.current].num);}
+			this.exec(this.ope[this.current], this.ope[this.current].num);
 			if(this.ope[this.current].property!='qsub' && this.ope[this.current].property!='color'){ this.anscount++;}
 			this.current++;
 
 			if(this.current<this.ope.length && !this.ope[this.current].chain){ break;}
 		}
-		if(this.reqReset==1){ room.resetRarea(); this.reqReset=0;}
-		this.enableRecord(); pc.paint(this.range.x1, this.range.y1, this.range.x2, this.range.y2);
+		this.postproc();
+		this.redoExec = false;
+		if(this.ope.length==0){ kc.inREDO=false;}
+	},
+	postproc : function(){
+		if(this.reqReset){
+			this.reqReset=false;
+
+			base.resetInfo();
+			base.resize_canvas();
+		}
+		this.enableRecord();
 		this.enb_btn();
+		pc.paint(this.range.x1, this.range.y1, this.range.x2, this.range.y2);
 	},
 	exec : function(ope, num){
 		var pp = ope.property;
@@ -167,16 +173,19 @@ UndoManager.prototype = {
 			else if(pp == 'qsub'){ bd.sQsC(ope.id, num);}
 			else if(pp == 'numobj'){ bd.cell[ope.id].numobj = num;}
 			else if(pp == 'numobj2'){ bd.cell[ope.id].numobj2 = num;}
+			else if(pp == 'cell' && !!num){ bd.cell[ope.id] = num;}
 			this.paintStack(bd.cell[ope.id].cx, bd.cell[ope.id].cy, bd.cell[ope.id].cx, bd.cell[ope.id].cy);
 		}
 		else if(ope.obj == 'excell'){
 			if     (pp == 'qnum'){ bd.sQnE(ope.id, num);}
 			else if(pp == 'direc'){ bd.sDiE(ope.id, num);}
+			else if(pp == 'excell' && !!num){ bd.excell[ope.id] = num;}
 		}
 		else if(ope.obj == 'cross'){
 			if     (pp == 'ques'){ bd.sQuX(ope.id, num);}
 			else if(pp == 'qnum'){ bd.sQnX(ope.id, num);}
 			else if(pp == 'numobj'){ bd.cross[ope.id].numobj = num;}
+			else if(pp == 'cross' && !!num){ bd.cross[ope.id] = num;}
 			this.paintStack(bd.cross[ope.id].cx-1, bd.cross[ope.id].cy-1, bd.cross[ope.id].cx, bd.cross[ope.id].cy);
 		}
 		else if(ope.obj == 'border'){
@@ -185,28 +194,26 @@ UndoManager.prototype = {
 			else if(pp == 'qans'){ bd.sQaB(ope.id, num);}
 			else if(pp == 'qsub'){ bd.sQsB(ope.id, num);}
 			else if(pp == 'line'){ bd.sLiB(ope.id, num);}
-			else if(pp == 'color'){ bd.border[ope.id].color = num;}
+			else if(pp == 'border' && !!num){ bd.border[ope.id] = num;}
 			this.paintBorder(ope.id);
 		}
 		else if(ope.obj == 'board'){
-			if     (pp == 'expandup'){ if(num==1){ menu.ex.expandup();}else{ menu.ex.reduceup();} }
-			else if(pp == 'expanddn'){ if(num==1){ menu.ex.expanddn();}else{ menu.ex.reducedn();} }
-			else if(pp == 'expandlt'){ if(num==1){ menu.ex.expandlt();}else{ menu.ex.reducelt();} }
-			else if(pp == 'expandrt'){ if(num==1){ menu.ex.expandrt();}else{ menu.ex.reducert();} }
-			else if(pp == 'reduceup'){ if(num==1){ menu.ex.reduceup();}else{ menu.ex.expandup();} }
-			else if(pp == 'reducedn'){ if(num==1){ menu.ex.reducedn();}else{ menu.ex.expanddn();} }
-			else if(pp == 'reducelt'){ if(num==1){ menu.ex.reducelt();}else{ menu.ex.expandlt();} }
-			else if(pp == 'reducert'){ if(num==1){ menu.ex.reducert();}else{ menu.ex.expandrt();} }
+			if     (pp == 'expandup'){ if(num==1){ menu.ex.expand('up');}else{ menu.ex.reduce('up');} }
+			else if(pp == 'expanddn'){ if(num==1){ menu.ex.expand('dn');}else{ menu.ex.reduce('dn');} }
+			else if(pp == 'expandlt'){ if(num==1){ menu.ex.expand('lt');}else{ menu.ex.reduce('lt');} }
+			else if(pp == 'expandrt'){ if(num==1){ menu.ex.expand('rt');}else{ menu.ex.reduce('rt');} }
+			else if(pp == 'reduceup'){ if(num==1){ menu.ex.reduce('up');}else{ menu.ex.expand('up');} }
+			else if(pp == 'reducedn'){ if(num==1){ menu.ex.reduce('dn');}else{ menu.ex.expand('dn');} }
+			else if(pp == 'reducelt'){ if(num==1){ menu.ex.reduce('lt');}else{ menu.ex.expand('lt');} }
+			else if(pp == 'reducert'){ if(num==1){ menu.ex.reduce('rt');}else{ menu.ex.expand('rt');} }
 
-			else if(pp == 'flipy'){ menu.ex.flipy(0,0,k.qcols-1,k.qrows-1);}
-			else if(pp == 'flipx'){ menu.ex.flipx(0,0,k.qcols-1,k.qrows-1);}
-			else if(pp == 'turnr'){ if(num==1){ menu.ex.turnr(0,0,k.qcols-1,k.qrows-1);} else{ menu.ex.turnl(0,0,k.qcols-1,k.qrows-1);} }
-			else if(pp == 'turnl'){ if(num==1){ menu.ex.turnl(0,0,k.qcols-1,k.qrows-1);} else{ menu.ex.turnr(0,0,k.qcols-1,k.qrows-1);} }
+			else if(pp == 'flipy'){ menu.ex.turnflip(1,{x1:0,y1:0,x2:k.qcols-1,y2:k.qrows-1});}
+			else if(pp == 'flipx'){ menu.ex.turnflip(2,{x1:0,y1:0,x2:k.qcols-1,y2:k.qrows-1});}
+			else if(pp == 'turnr'){ menu.ex.turnflip((num==1?3:4),{x1:0,y1:0,x2:k.qcols-1,y2:k.qrows-1}); }
+			else if(pp == 'turnl'){ menu.ex.turnflip((num==1?4:3),{x1:0,y1:0,x2:k.qcols-1,y2:k.qrows-1}); }
 
-			tc.Adjust();
-			base.resize_canvas();
 			this.range = { x1:0, y1:0, x2:k.qcols-1, y2:k.qrows-1};
-			this.reqReset = 1;
+			this.reqReset = true;
 		}
 	},
 	//---------------------------------------------------------------------------
