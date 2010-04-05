7998bfa5dd03ebc90d28ee8dd28cd51e2a22851b
saboten <none@none>
2010-04-05 11:58:58 +0900

    p.html ・テスト用のためのpzprBase.jsファイルpzprBase_develop.jsファイルを追加した
    --HG--
    branch : develop

---
+++ src/pzprBase_develop.js
@@ -0,0 +1,17 @@
+// pzplBase.js テスト用
+
+var pzprversion="v3.3.0pre";
+document.writeln("<script type=\"text/javascript\" src=\"src/pzprBase/Camp.js\"></script>");
+document.writeln("<script type=\"text/javascript\" src=\"src/pzprBase/global.js\"></script>");
+document.writeln("<script type=\"text/javascript\" src=\"src/pzprBase/Board.js\"></script>");
+document.writeln("<script type=\"text/javascript\" src=\"src/pzprBase/Graphic.js\"></script>");
+document.writeln("<script type=\"text/javascript\" src=\"src/pzprBase/MouseInput.js\"></script>");
+document.writeln("<script type=\"text/javascript\" src=\"src/pzprBase/KeyInput.js\"></script>");
+document.writeln("<script type=\"text/javascript\" src=\"src/pzprBase/Encode.js\"></script>");
+document.writeln("<script type=\"text/javascript\" src=\"src/pzprBase/Filesys.js\"></script>");
+document.writeln("<script type=\"text/javascript\" src=\"src/pzprBase/Answer.js\"></script>");
+document.writeln("<script type=\"text/javascript\" src=\"src/pzprBase/Undo.js\"></script>");
+document.writeln("<script type=\"text/javascript\" src=\"src/pzprBase/Menu.js\"></script>");
+document.writeln("<script type=\"text/javascript\" src=\"src/pzprBase/MenuExec.js\"></script>");
+document.writeln("<script type=\"text/javascript\" src=\"src/pzprBase/pzprUtil.js\"></script>");
+document.writeln("<script type=\"text/javascript\" src=\"src/pzprBase/Main.js\"></script>");
