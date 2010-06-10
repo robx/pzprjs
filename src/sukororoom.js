93aa43650cf01080e57b20da0d14b8976b426d5e
dk22 <none@none>
2010-06-11 03:11:50 +0900

    sukororoom.js ★数コロ部屋用スクリプトに対応しました
    --HG--
    branch : develop

---
+++ src/sukororoom.js
@@ -0,0 +1,189 @@
+//
+// パズル固有スクリプト部 数コロ部屋版 sukororoom.js v3.3.1
+//
+Puzzles.sukororoom = function(){ };
+Puzzles.sukororoom.prototype = {
+	setting : function(){
+		// グローバル変数の初期設定
+		if(!k.qcols){ k.qcols = 8;}	// 盤面の横幅
+		if(!k.qrows){ k.qrows = 8;}	// 盤面の縦幅
+		k.irowake  = 0;		// 0:色分け設定無し 1:色分けしない 2:色分けする
+
+		k.iscross  = 0;		// 1:盤面内側のCrossがあるパズル 2:外枠上を含めてCrossがあるパズル
+		k.isborder = 1;		// 1:Border/Lineが操作可能なパズル 2:外枠上も操作可能なパズル
+		k.isexcell = 0;		// 1:上・左側にセルを用意するパズル 2:四方にセルを用意するパズル
+
+		k.isLineCross     = false;	// 線が交差するパズル
+		k.isCenterLine    = false;	// マスの真ん中を通る線を回答として入力するパズル
+		k.isborderAsLine  = false;	// 境界線をlineとして扱う
+		k.hasroom         = true;	// いくつかの領域に分かれている/分けるパズル
+		k.roomNumber      = false;	// 部屋の問題の数字が1つだけ入るパズル
+
+		k.dispzero        = false;	// 0を表示するかどうか
+		k.isDispHatena    = true;	// qnumが-2のときに？を表示する
+		k.isAnsNumber     = true;	// 回答に数字を入力するパズル
+		k.NumberWithMB    = true;	// 回答の数字と○×が入るパズル
+		k.linkNumber      = true;	// 数字がひとつながりになるパズル
+
+		k.BlackCell       = false;	// 黒マスを入力するパズル
+		k.NumberIsWhite   = false;	// 数字のあるマスが黒マスにならないパズル
+		k.RBBlackCell     = false;	// 連黒分断禁のパズル
+		k.checkBlackCell  = false;	// 正答判定で黒マスの情報をチェックするパズル
+		k.checkWhiteCell  = false;	// 正答判定で白マスの情報をチェックするパズル
+
+		k.ispzprv3ONLY    = false;	// ぱずぷれアプレットには存在しないパズル
+		k.isKanpenExist   = false;	// pencilbox/カンペンにあるパズル
+
+		base.setTitle("数コロ部屋","Sukoro-room");
+		base.setExpression("　マスのクリックやキーボードで数字を入力できます。QAZキーで○、WSXキーで×を入力できます。",
+					   " It is available to input number by keybord or mouse. Each QAZ key to input auxiliary circle, each WSX key to input auxiliary cross.");
+		base.setFloatbgcolor("rgb(64, 64, 64)");
+	},
+	menufix : function(){ },
+
+	//---------------------------------------------------------
+	//入力系関数オーバーライド
+	input_init : function(){
+		// マウス入力系
+		mv.mousedown = function(){
+			if(k.editmode) this.inputborder();
+			if(k.playmode){
+				if(!kp.enabled()){ this.inputqnum();}
+				else{ kp.display();}
+			}
+		};
+		mv.mouseup = function(){
+			if(this.notInputted()){
+				if(k.editmode){
+					if(!kp.enabled()){ this.inputqnum();}
+					else{ kp.display();}
+				}
+			}
+		};
+		mv.mousemove = function(){
+			if(k.editmode && this.btn.Left) this.inputborder();
+		};
+
+		// キーボード入力系
+		kc.keyinput = function(ca){
+			if(this.moveTCell(ca)){ return;}
+			this.key_sukoro(ca);
+		};
+		kc.key_sukoro = function(ca){
+			if(k.playmode){
+				var cc=tc.getTCC();
+				if     (ca==='q'||ca==='a'||ca==='z')          { ca=(bd.QsC(cc)===1?'1':'s1');}
+				else if(ca==='w'||ca==='s'||ca==='x')          { ca=(bd.QsC(cc)===2?'2':'s2');}
+				else if(ca==='e'||ca==='d'||ca==='c'||ca==='-'){ ca=' '; }
+				else if(ca==='1' && bd.AnC(cc)===1)            { ca='s1';}
+				else if(ca==='2' && bd.AnC(cc)===2)            { ca='s2';}
+			}
+			this.key_inputqnum(ca);
+		};
+
+		kp.kpgenerate = function(mode){
+			this.inputcol('num','knum1','1','1');
+			this.inputcol('num','knum2','2','2');
+			this.inputcol('num','knum3','3','3');
+			this.inputcol('num','knum4','4','4');
+			this.insertrow();
+			if(mode==1){
+				this.inputcol('num','knum.','-','?');
+				this.inputcol('num','knum_',' ',' ');
+				this.inputcol('empty','knumx','','');
+				this.inputcol('empty','knumy','','');
+				this.insertrow();
+			}
+			else{
+				this.tdcolor = pc.mbcolor;
+				this.inputcol('num','knumq','q','○');
+				this.inputcol('num','knumw','w','×');
+				this.tdcolor = "black";
+				this.inputcol('num','knum_',' ',' ');
+				this.inputcol('empty','knumx','','');
+				this.insertrow();
+			}
+		};
+		kp.generate(kp.ORIGINAL, true, true, kp.kpgenerate);
+		kp.kpinput = function(ca){
+			if(kc.key_sukoro(ca)){ return;}
+			kc.key_inputqnum(ca);
+		};
+
+		bd.maxnum = 4;
+	},
+
+	//---------------------------------------------------------
+	//画像表示系関数オーバーライド
+	graphic_init : function(){
+
+		pc.paint = function(x1,y1,x2,y2){
+			this.drawBGCells(x1,y1,x2,y2);
+			this.drawGrid(x1,y1,x2,y2);
+
+			this.drawBorders(x1,y1,x2,y2);
+
+			this.drawMBs(x1,y1,x2,y2);
+			this.drawNumbers(x1,y1,x2,y2);
+
+			this.drawChassis(x1,y1,x2,y2);
+
+			this.drawCursor(x1,y1,x2,y2);
+		};
+	},
+
+	//---------------------------------------------------------
+	// URLエンコード/デコード処理
+	encode_init : function(){
+		enc.pzlimport = function(type){
+			this.decodeBorder();
+			this.decodeNumber10();
+		};
+		enc.pzlexport = function(type){
+			this.encodeBorder();
+			this.encodeNumber10();
+		};
+
+		//---------------------------------------------------------
+		fio.decodeData = function(){
+			this.decodeBorderQues();
+			this.decodeCellQnum();
+			this.decodeCellAnumsub();
+		};
+		fio.encodeData = function(){
+			this.encodeBorderQues();
+			this.encodeCellQnum();
+			this.encodeCellAnumsub();
+		};
+	},
+
+	//---------------------------------------------------------
+	// 正解判定処理実行部
+	answer_init : function(){
+		ans.checkAns = function(){
+
+			var rinfo = area.getRoomInfo();
+			if( !this.checkDifferentNumberInRoom(rinfo, bd.getNum) ){
+				this.setAlert('1つの部屋に同じ数字が複数入っています。','A room has two or more same numbers.'); return false;
+			}
+
+			if( !this.checkSameObjectInRoom(rinfo, function(c){ return (area.isBlock(c)?1:2);}) ){
+				this.setAlert('数字のあるなしが混在した部屋があります。','A room includes both numbered and non-numbered cells.'); return false;
+			}
+
+			if( !this.checkDir4Cell(area.isBlock,0) ){
+				this.setAlert('数字と、その数字の上下左右に入る数字の数が一致していません。','The number of numbers placed in four adjacent cells is not equal to the number.'); return false;
+			}
+
+			if( !this.checkOneArea( area.getNumberInfo() ) ){
+				this.setAlert('タテヨコにつながっていない数字があります。','Numbers are devided.'); return false;
+			}
+
+			if( !this.checkAllCell(function(c){ return (bd.QsC(c)===1);}) ){
+				this.setAlert('数字の入っていないマスがあります。','There is a cell that is not filled in number.'); return false;
+			}
+
+			return true;
+		};
+	}
+};
