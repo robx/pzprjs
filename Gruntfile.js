b7e31c218085300717c2cc0efef1bf1bd2bf7319
sabo2 <none@none>
2014-01-26 21:25:57 +0900

    pzpr.jsと各パズル野ファイルを結合したpzpr-all.jsを生成するようにした

--- Gruntfile.js
+++ Gruntfile.js
@@ -55,12 +55,24 @@ module.exports = function(grunt){
         files: [
           { src: [], dest: 'dist/pzpr.concat.js' }
         ]
+      },
+      'pzpr-all': {
+        files: [
+          { src: [], dest: 'dist/pzpr-all.concat.js' }
+        ]
       }
     },
 
     replace: {
       pzpr: {
         src: 'dist/pzpr.concat.js',
+        overwrite: true,
+        replacements: [
+          { from: "<deploy-version>", to: "<%= pkg.version %>"}
+        ]
+      },
+      'pzpr-all': {
+        src: 'dist/pzpr-all.concat.js',
         overwrite: true,
         replacements: [
           { from: "<deploy-version>", to: "<%= pkg.version %>"}
@@ -79,6 +91,11 @@ module.exports = function(grunt){
       pzpr: {
         files : [
           { src: 'dist/pzpr.concat.js', dest: 'dist/pzpr.js' }
+        ]
+      },
+      'pzpr-all': {
+        files : [
+          { src: 'dist/pzpr-all.concat.js', dest: 'dist/pzpr-all.js' }
         ]
       },
       'pzpr-debug': {
@@ -108,7 +125,12 @@ module.exports = function(grunt){
       },
       pzpr: {
         files: [
-          { src: 'dist/pzpr.concat.js', dest: 'dist/pzpr.js' }
+          { src: 'dist/pzpr.concat.js', dest: 'dist/pzpr.js' },
+        ]
+      },
+      'pzpr-all': {
+        files: [
+          { src: 'dist/pzpr-all.concat.js', dest: 'dist/pzpr-all.js' },
         ]
       },
       puzzle: {
@@ -133,13 +155,25 @@ module.exports = function(grunt){
   var prop = "concat.pzpr.files.0.src";
   grunt.config.set(prop, wrap(component.map(mod2file)));
   
+  prop = "concat.pzpr-all.files.0.src";
+  grunt.config.set(prop, wrap(component.map(mod2file).concat(['src/puzzle/*.js'])));
+  
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-text-replace');
   
-  grunt.registerTask('default', ['clean', 'copy:pzpr-debug', 'replace:pzpr-debug',      'copy:puzzle',   'copy:image']);
-  grunt.registerTask('combine', ['clean', 'concat:pzpr', 'replace:pzpr', 'copy:pzpr',   'copy:puzzle',   'copy:image']);
-  grunt.registerTask('release', ['clean', 'concat:pzpr', 'replace:pzpr', 'uglify:pzpr', 'uglify:puzzle', 'copy:image']);
+  grunt.registerTask('default', ['clean', 'copy:pzpr-debug', 'replace:pzpr-debug', 'copy:puzzle', 'copy:image']);
+  
+  grunt.registerTask('combine-pzpr',     ['concat:pzpr', 'replace:pzpr', 'copy:pzpr'])
+  grunt.registerTask('combine-pzpr-all', ['concat:pzpr-all', 'replace:pzpr-all', 'copy:pzpr-all'])
+  grunt.registerTask('combine-puzzle',   ['copy:puzzle', 'copy:image'])
+  
+  grunt.registerTask('release-pzpr',     ['concat:pzpr', 'replace:pzpr', 'uglify:pzpr'])
+  grunt.registerTask('release-pzpr-all', ['concat:pzpr-all', 'replace:pzpr-all', 'uglify:pzpr-all'])
+  grunt.registerTask('release-puzzle',   ['uglify:puzzle', 'copy:image'])
+  
+  grunt.registerTask('combine', ['clean', 'combine-puzzle', 'combine-pzpr', 'combine-pzpr-all']);
+  grunt.registerTask('release', ['clean', 'release-puzzle', 'release-pzpr', 'release-pzpr-all']);
 };
