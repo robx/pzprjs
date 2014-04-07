898b2bc7d03103531078ed6c7f3e3d9702d42200
sabo2 <none@none>
2014-04-07 23:21:12 +0900

    debug用pzprv3.jsが参照するソースコードの配置を固定にしました

--- src/js/pzprv3-all.js
+++ src/js/pzprv3-all.js
@@ -121,17 +121,8 @@
 	catch(e){}
 	
 	if(isbrowser){
-		var dir = (function getpath(){
-			var srcs=document.getElementsByTagName('script');
-			for(var i=0;i<srcs.length;i++){
-				var result = srcs[i].src.match(/^(.*\/)pzprv3\-all\.js$/);
-				if(result){ return result[1] + (!result[1].match(/\/$/) ? '/' : '');}
-			}
-			return "";
-		})();
-		
 		for(var i=0;i<files.length;i++){
-			document.write('<script type="text/javascript" src="'+dir+files[i]+'"></script>');
+			document.write('<script type="text/javascript" src="./js/'+files[i]+'.js"></script>');
 		}
 	}
 	else{ exports.component = files;}
