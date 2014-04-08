93eb245da0106411f8c927f9b901b351ed12a483
sabo2 <none@none>
2014-04-08 22:35:46 +0900

    puzzle: toBlob, toDataURLをcanvas.jsに取り込んでいたのにコードが残っていたので、リファクタリングしました

--- src/js/puzzle/Puzzle.js
+++ src/js/puzzle/Puzzle.js
@@ -291,25 +291,13 @@ pzpr.Puzzle.prototype =
 	// owner.generateLocalCanvas() 上記関数の共通処理
 	//---------------------------------------------------------------------------
 	toDataURL : function(type, cellsize){
-		type = (!!type ? type : "");
-		if(!type.match(/svg/)){ return this.getLocalCanvas(this.imgcanvas[0], cellsize).toDataURL();}
-		else{ return "data:image/svg+xml;base64," + window.btoa(this.getLocalCanvas(this.imgcanvas[1], cellsize).innerHTML);}
+		return this.getLocalCanvas((type||""), cellsize).toDataURL();
 	},
 	toBlob : function(type, cellsize){
-		type = (!!type ? type : "");
-		if(!type.match(/svg/)){
-			try{ return this.getLocalCanvas(this.imgcanvas[0], cellsize).toBlob();}
-			catch(e){}
-			/* Webkit, BlinkにtoBlobがない... */
-			this.getLocalCanvas(this.imgcanvas[0], cellsize).toDataURL().match(/data:(.*);base64,(.*)/);
-			var bin = window.atob(RegExp.$2);
-			var buf = new Uint8Array(bin.length);
-			for(var i=0,len=buf.length;i<len;i++){ buf[i]=bin.charCodeAt(i);}
-			return new Blob([buf.buffer], {type:RegExp.$1});
-		}
-		else{ return new Blob([this.getLocalCanvas(this.imgcanvas[1], cellsize).innerHTML], {type:'image/svg+xml'});}
+		return this.getLocalCanvas((type||""), cellsize).toBlob();
 	},
-	getLocalCanvas : function(el, cellsize){
+	getLocalCanvas : function(type, cellsize){
+		var el = this.imgcanvas[type.match(/svg/)?1:0];
 		var pc2 = new this.Graphic();
 		pc2.initCanvas(el);
 		pc2.outputImage = true;		/* 一部画像出力時に描画しないオブジェクトがあるパズル向け設定 */
