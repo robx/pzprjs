aa202d1b04245d5a16516a388a9ab18fb3189156
sabo2 <none@none>
2014-05-25 22:01:13 +0900

    tests: ループ系パズルの入力テストを追加しました

--- tests/script/test_ringring.js
+++ tests/script/test_ringring.js
@@ -10,5 +10,14 @@ ui.debug.addDebugData('ringring', {
 		['lnNotRect',"pzprv3/ringring/5/5/1 0 0 1 1 /0 0 0 0 0 /0 0 0 1 0 /0 0 0 1 0 /0 0 0 0 0 /0 1 0 0 /1 0 1 1 /0 0 0 0 /0 0 0 0 /1 1 1 1 /0 1 1 0 0 /1 0 0 0 1 /1 0 0 0 1 /1 0 -1 0 1 /"],
 		['ceEmpty',  "pzprv3/ringring/5/5/1 0 0 1 1 /0 0 0 0 0 /0 0 0 1 0 /0 0 0 1 0 /0 0 0 0 0 /0 1 0 0 /1 1 1 1 /0 1 0 0 /0 0 0 0 /1 1 1 1 /0 1 1 0 0 /1 1 1 0 1 /1 0 0 0 1 /1 0 -1 0 1 /"],
 		[null,       "pzprv3/ringring/5/5/1 0 0 1 1 /0 0 0 0 0 /0 0 0 1 0 /0 0 0 1 0 /0 0 0 0 0 /0 1 0 0 /1 1 1 1 /0 0 0 0 /0 1 0 0 /1 1 1 1 /0 1 1 0 0 /1 1 1 0 1 /1 1 1 0 1 /1 0 -1 0 1 /"]
+	],
+	inputs : [
+		/* 問題入力テスト */
+		{ input:["newboard,3,3", "editmode"] },
+		{ input:["mouse,left, 1,1", "mouse,leftx2, 3,1", "mouse,right, 1,3", "mouse,rightx2, 3,3"],
+		  result:"pzprv3/ringring/3/3/1 0 0 /1 0 0 /0 0 0 /0 0 /0 0 /0 0 /0 0 0 /0 0 0 /" },
+		/* 回答入力テスト */
+		{ input:["newboard,3,3", "editmode", "mouse,left, 3,3", "playmode", "mouse,left, 1,3, 5,3, 5,5, 3,5, 3,1"],
+		  result:"pzprv3/ringring/3/3/0 0 0 /0 1 0 /0 0 0 /0 0 /0 0 /0 1 /0 0 0 /0 0 1 /" }
 	]
 });
