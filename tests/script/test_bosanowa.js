366a2cd554cde8978159fbc4d68e881c10561133
sabo2 <none@none>
2014-09-14 02:34:36 +0900

    tests_bosanowa: テストに時間がかかりすぎて警告が出るので、時間がかからないようにテストを修正します

--- tests/script/test_bosanowa.js
+++ tests/script/test_bosanowa.js
@@ -17,9 +17,11 @@ ui.debug.addDebugData('bosanowa', {
 		  result:"pzprv3/bosanowa/1/5/0 1 2 3 10 /0 . . . . /. . . . /" },
 		{ input:["cursor,3,1", "key,-", "key,right,right,-,-", "key,right,right, " ],
 		  result:"pzprv3/bosanowa/1/5/0 0 0 0 10 /0 0 0 0 . /. . . . /" },
-		{ input:["newboard,5,2", "editmode", "cursor,5,1", "key,w"] },
-		{ input:["cursor,0,0", "mouse,leftx2, 1,1",  "mouse,leftx3, 3,1",  "mouse,leftx256, 5,1",  "mouse,leftx257, 7,1",  "mouse,leftx258, 9,1",
-							   "mouse,rightx2, 1,3", "mouse,rightx3, 3,3", "mouse,rightx256, 5,3", "mouse,rightx257, 7,3", "mouse,rightx258, 9,3"],
+		{ input:["newboard,5,2", "editmode"] },
+		{ input:["cursor,5,1", "key,2,4,9", "key,right,right,2,4,9", "key,right,right,2,4,9"] },
+		{ input:["cursor,5,3", "key,6", "key,right,right,6", "key,right,right,6"] },
+		{ input:["cursor,0,0", "mouse,leftx2, 1,1",  "mouse,leftx3, 3,1",  "mouse,leftx6, 5,1",  "mouse,leftx7, 7,1",  "mouse,leftx8, 9,1",
+							   "mouse,rightx2, 1,3", "mouse,rightx3, 3,3", "mouse,rightx6, 5,3", "mouse,rightx7, 7,3", "mouse,rightx8, 9,3"],
 		  result:"pzprv3/bosanowa/2/5/0 1 254 255 . /255 254 1 0 . /0 . . . . /. . . 0 . /. . . . /. . . . /. . . . . /" },
 		{ input:["cursor,2,1", "key,1,2", "key,right,right,1,2", "cursor,8,1", "key,0" ],
 		  result:"pzprv3/bosanowa/2/5/0 1 254 255 . /255 254 1 0 . /0 . . . . /. . . 0 . /12 12 . . /. . . . /. . . . . /" },
@@ -35,8 +37,10 @@ ui.debug.addDebugData('bosanowa', {
 		{ input:["newboard,5,2", "editmode"] },
 		{ input:["cursor,1,1", "key,w", "key,right,right,w", "key,right,right",   "key,right,right,w", "key,right,right,w",
 				 "cursor,1,3", "key,w", "key,right,right,w", "key,right,right,w", "key,right,right,w", "key,right,right,w", "playmode" ] },
-		{ input:["cursor,0,0", "mouse,leftx2, 1,1",  "mouse,leftx3, 3,1",  "mouse,leftx4, 5,1",  "mouse,leftx256, 7,1",  "mouse,leftx257, 9,1",
-							   "mouse,rightx2, 1,3", "mouse,rightx3, 3,3", "mouse,rightx4, 5,3", "mouse,rightx256, 7,3", "mouse,rightx257, 9,3"],
+		{ input:["cursor,7,1", "key,2,5,0", "key,right,right,2,5,0"] },
+		{ input:["cursor,7,3", "key,6", "key,right,right,6"] },
+		{ input:["cursor,0,0", "mouse,leftx2, 1,1",  "mouse,leftx3, 3,1",  "mouse,leftx4, 5,1",  "mouse,leftx6, 7,1",  "mouse,leftx7, 9,1",
+							   "mouse,rightx2, 1,3", "mouse,rightx3, 3,3", "mouse,rightx4, 5,3", "mouse,rightx6, 7,3", "mouse,rightx7, 9,3"],
 		  result:"pzprv3/bosanowa/2/5/0 0 0 0 0 /0 0 0 0 0 /1 2 3 255 0 /255 254 253 1 0 /. . . . /. . . . /. . . . . /" }
 	]
 });
