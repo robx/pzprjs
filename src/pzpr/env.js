bcd7514ab7dd550e6dd0661aa54a6579c4816f93
sabo2 <none@none>
2016-05-02 17:31:01 +0900

    env: Drop pzpr.env.storage property so as not to ask using local storage on Safari
    This property was used only for DataBase.js in pzprv3-ui
    and now it access only localStorage and doesn't check the property.

--- src/pzpr/env.js
+++ src/pzpr/env.js
@@ -12,9 +12,6 @@ pzpr.env = (function(){
 		Presto: (typeof window==='object' && !!window.opera)
 	};
 	
-	var Gecko = (UA.indexOf('Gecko')>-1 && UA.indexOf('KHTML')===-1);
-	var Gecko7orOlder = (Gecko && UA.match(/rv\:(\d+\.\d+)/) && +RegExp.$1<8.0); /* Firefox8.0よりも前 */
-	
 	var ios     = (UA.indexOf('like Mac OS X') > -1);
 	var android = (UA.indexOf('Android') > -1);
 	var os = {
@@ -22,27 +19,6 @@ pzpr.env = (function(){
 		Android: (android),
 		mobile : (ios || android)
 	};
-	
-	var storage = (function(){
-		var val = 0x00;
-		try{ if(!!window.sessionStorage){ val |= 0x10;}}catch(e){}
-		try{ if(!!window.localStorage)  { val |= 0x08;}}catch(e){}
-		try{ if(!!window.indexedDB)     { val |= 0x04;}}catch(e){}
-		try{ if(!!window.openDatabase){ // Opera10.50対策
-			var dbtmp = openDatabase('pzprv3_manage', '1.0', 'manager', 1024*1024*5);	// Chrome3対策
-			if(!!dbtmp){ val |= 0x02;}
-		}}catch(e){}
-		
-		// Firefox 8.0より前はローカルだとデータベース系は使えない
-		if(Gecko7orOlder && !location.hostname){ val = 0;}
-		
-		return {
-			session : !!(val & 0x10),
-			localST : !!(val & 0x08),
-			WebIDB  : !!(val & 0x04),
-			WebSQL  : !!(val & 0x02)
-		};
-	})();
 	
 	var api = {
 		touchevent      : isbrowser && ((!!window.ontouchstart) || (!!document.createTouch)),
@@ -54,7 +30,6 @@ pzpr.env = (function(){
 	return {
 		bz      : bz,
 		OS      : os,
-		storage : storage,
 		API     : api,
 		browser : isbrowser,
 		node    : pzpr.Candle.env.node
