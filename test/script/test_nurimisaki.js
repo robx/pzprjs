3409123ce5e05d77601bfd48e213864457ce12dd
Robert Vollmert <rob@vllmrt.net>
2019-03-13 23:29:20 +0100

    minimal test scripts for the new types

---
+++ test/script/test_nurimisaki.js
@@ -0,0 +1,10 @@
+/* test_nurimisaki.js */
+
+ui.debug.addDebugData('nurimisaki', {
+	url : '4/4/s4h',
+	failcheck : [
+		[null, "pzprv3/nurimisaki/4/4/. . . . /. . . . /. . . . /. 4 . . /# + + + /# + # + /# + + + /# + # # "]
+	],
+	inputs : [
+	]
+});
