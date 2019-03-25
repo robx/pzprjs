83367a4818a57053b394741dd99f66b8fb96a349
Robert Vollmert <rob@vllmrt.net>
2019-03-25 13:54:39 +0100

    test that rendering to svg doesn't throw

---
+++ test/puzzle/render_test.js
@@ -0,0 +1,19 @@
+// test/render.js
+// jshint node:true, browser:false, esnext:true
+/* global describe:false, it:false */
+
+var pzpr = require('../../dist/pzpr.js');
+
+var testdata = require('../load_testdata.js');
+
+pzpr.variety.each(function(pid){
+	describe(pid+' render test', function(){
+		it('render svg', function(){
+			var puzzle = new pzpr.Puzzle();
+			puzzle.open(pid+'/'+testdata[pid].url, () => {
+				puzzle.toBuffer('svg', 0, 30);
+			});
+		});
+	});
+});
+
