8e6af164116dd2044c146f1addf1af2cdbb5928c
Robert Vollmert <rob@vllmrt.net>
2019-04-22 21:02:11 +0200

    heteromino: assert that we can't draw between invalid cells

--- test/script/test_heteromino.js
+++ test/script/test_heteromino.js
@@ -9,5 +9,9 @@ ui.debug.addDebugData('heteromino', {
 		[null,          "pzprv3/heteromino/5/5/. . . . * /. . * . * /. . * . . /* * . . . /* . . . . /0 0 1 0 0 0 /0 1 0 0 0 0 /0 0 0 0 1 0 /0 0 0 0 1 0 /0 0 0 0 1 0 /0 0 0 0 0 /-1 1 0 0 0 /1 0 0 1 0 /0 0 0 -1 0 /0 0 1 1 0 /0 0 0 0 0 /"]
 	],
 	inputs : [
+		{ input: ["newboard,3,2", "editmode", "mouse,left, 1,1", "mouse,left, 3,1", "mouse,left, 1,3"],
+		  result: "pzprv3/heteromino/2/3/* * . /* . . /0 0 0 0 /0 0 0 0 /0 0 0 /0 0 0 /0 0 0 /"},
+		{ input: ["playmode", "mouse,left, 0,0 0,2", "mouse,left, 2,0, 2,2", "mouse,left, 4,0, 4,2", "mouse,left, 6,0, 6,2", "mouse,left, 4,2, 4,4"],
+		  result: "pzprv3/heteromino/2/3/* * . /* . . /0 0 0 0 /0 0 1 0 /0 0 0 /0 0 0 /0 0 0 /"}
 	]
 });
