aa6e841909b9b3144cacdb6deecbf910589080fe
x-sheep <x-sheep@users.noreply.github.com>
2019-04-22 18:24:36 +0200

    Add Pencils unit test puzzle

---
+++ test/script/test_pencils.js
@@ -0,0 +1,12 @@
+/* test_pencils.js */
+
+ui.debug.addDebugData('pencils', {
+	url : '5/5/q3k1ngkh3q',
+	failcheck : [
+		// TODO add failure cases
+		[null, "pzprv3/pencils/5/5/. . . . . /. . o3 . o1 /. . . . 1 /. 2 o3 . . /. . . . . /0 0 0 0 /1 1 1 1 /1 1 1 0 /0 1 1 1 /0 1 0 1 /0 1 1 0 1 /0 0 0 0 1 /0 0 0 0 0 /1 1 1 0 0 /1 1 0 1 /0 0 0 0 /0 0 0 1 /1 0 0 0 /0 0 1 0 /1 0 0 0 0 /0 0 0 1 0 /1 0 0 0 0 /0 0 0 1 0 /. . 1 . 1 /. . . . . /. . . . . /. . . . . /. . 4 . . /"]
+	],
+	inputs : [
+		// TODO add input tests
+	]
+});
