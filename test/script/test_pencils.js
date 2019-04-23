61f869aed2ae6d0a87361fa6ce0516edf1091102
Lennard Sprong <x-sheep@users.noreply.github.com>
2019-04-23 19:37:34 +0200

    Add input tests

--- test/script/test_pencils.js
+++ test/script/test_pencils.js
@@ -25,6 +25,22 @@ ui.debug.addDebugData('pencils', {
 		[null, "pzprv3/pencils/5/5/. . . . . /. . o3 . o1 /. . . . 1 /. 2 o3 . . /. . . . . /0 0 0 0 /1 1 1 1 /1 1 1 0 /0 1 1 1 /0 1 0 1 /0 1 1 0 1 /0 0 0 0 1 /0 0 0 0 0 /1 1 1 0 0 /1 1 0 1 /0 0 0 0 /0 0 0 1 /1 0 0 0 /0 0 1 0 /1 0 0 0 0 /0 0 0 1 0 /1 0 0 0 0 /0 0 0 1 0 /. . 1 . 1 /. . . . . /. . . . . /. . . . . /. . 4 . . /"]
 	],
 	inputs : [
-		// TODO add input tests
+		{ input:["newboard,3,2", "editmode"],
+		result:"pzprv3/pencils/2/3/. . . /. . . /0 0 /0 0 /0 0 0 /0 0 /0 0 /0 0 0 /. . . /. . . /" },
+		{ input:["mouse,left, 1,3, -1,3", "mouse,left, 5,3, 7,3", "cursor,3,3", "key,shift+down", "cursor,3,1", "key,shift+up"],
+		result:"pzprv3/pencils/2/3/. 1 . /3 2 4 /0 0 /0 0 /0 0 0 /0 0 /0 0 /0 0 0 /. . . /. . . /" },
+		{ input:["cursor,3,3", "key,-", "cursor,5,3", "key,1"],
+		result:"pzprv3/pencils/2/3/. 1 . /3 o-2 o1 /0 0 /0 0 /0 0 0 /0 0 /0 0 /0 0 0 /. . . /. . . /" },
+		{ input:["mouse,leftx3,1,1"],
+		result:"pzprv3/pencils/2/3/o2 1 . /3 o-2 o1 /0 0 /0 0 /0 0 0 /0 0 /0 0 /0 0 0 /. . . /. . . /" },
+
+		{ input:["newboard,3,2", "playmode"] },
+
+		{ input:["mouse,left,2,0,2,2", "mouse,left,1,1,5,1"],
+		result:"pzprv3/pencils/2/3/. . . /. . . /1 0 /0 0 /0 0 0 /0 1 /0 0 /0 0 0 /. 4 . /. . . /" },
+		{ input:["mouse,left,1,1,3,1"],
+		result:"pzprv3/pencils/2/3/. . . /. . . /1 0 /0 0 /0 0 0 /0 1 /0 0 /0 0 0 /. . . /. . . /" },
+		{ input:["mouse,left,3,1,1,1"],
+		result:"pzprv3/pencils/2/3/. . . /. . . /1 0 /0 0 /0 0 0 /0 1 /0 0 /0 0 0 /. 4 . /. . . /" }
 	]
 });
