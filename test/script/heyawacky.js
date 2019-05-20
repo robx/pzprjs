ec68f686a20ec558a9e1b189efac6236a55dc59a
Robert Vollmert <rob@vllmrt.net>
2019-05-20 11:37:30 +0200

    rename script test files

---
+++ test/script/heyawacky.js
@@ -0,0 +1,10 @@
+/* heyawacky.js */
+
+ui.debug.addDebugData('heyawacky', {
+	url : '4/4/3nocigj1',
+	failcheck : [
+		[null, "pzprv3/heyawacky/4/4/5/0 0 0 0 /0 1 2 0 /0 0 2 0 /3 0 4 0 /. . . . /. . . . /. . . . /. . 1 . /+ + + + /+ + # + /+ # + + /+ + # + "]
+	],
+	inputs : [
+	]
+});
