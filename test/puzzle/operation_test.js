aba91e81ba462d42fc949d14494029a575d6a79b
Robert Vollmert <rob@vllmrt.net>
2019-12-13 12:02:51 +0100

    opemgr: check current behaviour for cycling through numbers (issue #93)

--- test/puzzle/operation_test.js
+++ test/puzzle/operation_test.js
@@ -187,4 +187,90 @@ describe('Trial mode test', function(){
 		assert.equal(puzzle.opemgr.position, 1);
 		assert.equal(puzzle.opemgr.history.length, 1);
 	});
+
+	// Testing cyclic number input: https://github.com/robx/pzprjs/issues/93
+	it('Number input test, cycle empty -> empty', function(){
+		var puzzle = new pzpr.Puzzle().open('building/3/3');
+		puzzle.setMode('play');
+
+		puzzle.mouse.inputPath(3,3); // select center cell
+		assert.equal(puzzle.cursor.bx, 3);
+		assert.equal(puzzle.cursor.by, 3);
+		assert.equal(puzzle.board.getc(3,3).anum, -1)
+		assert.equal(puzzle.opemgr.history.length, 0);
+
+		puzzle.mouse.inputPath(3,3); // click -> 1
+		assert.equal(puzzle.board.getc(3,3).anum, 1)
+		assert.equal(puzzle.opemgr.history.length, 1);
+		assert.equal(puzzle.opemgr.history[0][0].old, -1);
+		assert.equal(puzzle.opemgr.history[0][0].num, 1);
+
+		puzzle.mouse.inputPath(3,3); // click -> 2
+		assert.equal(puzzle.board.getc(3,3).anum, 2)
+		assert.equal(puzzle.opemgr.history.length, 1);
+		assert.equal(puzzle.opemgr.history[0][0].old, -1);
+		assert.equal(puzzle.opemgr.history[0][0].num, 2);
+
+		puzzle.mouse.inputPath(3,3); // click -> 3
+		assert.equal(puzzle.board.getc(3,3).anum, 3)
+		assert.equal(puzzle.opemgr.history.length, 1);
+		assert.equal(puzzle.opemgr.history[0][0].old, -1);
+		assert.equal(puzzle.opemgr.history[0][0].num, 3);
+
+		puzzle.mouse.inputPath(3,3); // click -> -1
+		assert.equal(puzzle.board.getc(3,3).anum, -1)
+		assert.equal(puzzle.opemgr.history.length, 1);
+		assert.equal(puzzle.opemgr.history[0][0].old, -1);
+		assert.equal(puzzle.opemgr.history[0][0].num, -1);
+	});
+
+	it('Number input test, cycle num -> num', function(){
+		var puzzle = new pzpr.Puzzle().open('building/3/3');
+		puzzle.setMode('play');
+
+		// first set things up so we can start cycling from a number
+
+		puzzle.mouse.inputPath(3,3); // select center cell
+		puzzle.key.inputKeys("1"); // and set it to 1
+		assert.equal(puzzle.cursor.bx, 3);
+		assert.equal(puzzle.cursor.by, 3);
+		assert.equal(puzzle.board.getc(3,3).anum, 1)
+		assert.equal(puzzle.opemgr.history.length, 1);
+
+		puzzle.mouse.inputPath(1,3); // select different cell
+		puzzle.key.inputKeys("2"); // and set it to some value
+		assert.equal(puzzle.opemgr.history.length, 2);
+
+		puzzle.mouse.inputPath(3,3); // switch back to center
+		assert.equal(puzzle.cursor.bx, 3);
+		assert.equal(puzzle.cursor.by, 3);
+		assert.equal(puzzle.board.getc(3,3).anum, 1)
+		assert.equal(puzzle.opemgr.history.length, 2);
+
+		// ready to start proper test
+
+		puzzle.mouse.inputPath(3,3); // click -> 2
+		assert.equal(puzzle.board.getc(3,3).anum, 2)
+		assert.equal(puzzle.opemgr.history.length, 3);
+		assert.equal(puzzle.opemgr.history[2][0].old, 1);
+		assert.equal(puzzle.opemgr.history[2][0].num, 2);
+
+		puzzle.mouse.inputPath(3,3); // click -> 3
+		assert.equal(puzzle.board.getc(3,3).anum, 3)
+		assert.equal(puzzle.opemgr.history.length, 3);
+		assert.equal(puzzle.opemgr.history[2][0].old, 1);
+		assert.equal(puzzle.opemgr.history[2][0].num, 3);
+
+		puzzle.mouse.inputPath(3,3); // click -> empty
+		assert.equal(puzzle.board.getc(3,3).anum, -1)
+		assert.equal(puzzle.opemgr.history.length, 3);
+		assert.equal(puzzle.opemgr.history[2][0].old, 1);
+		assert.equal(puzzle.opemgr.history[2][0].num, -1);
+
+		puzzle.mouse.inputPath(3,3); // click -> 1
+		assert.equal(puzzle.board.getc(3,3).anum, 1)
+		assert.equal(puzzle.opemgr.history.length, 3);
+		assert.equal(puzzle.opemgr.history[2][0].old, 1);
+		assert.equal(puzzle.opemgr.history[2][0].num, 1);
+	});
 });
