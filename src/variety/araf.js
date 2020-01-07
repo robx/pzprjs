84b7e89d497a21e76f84941bc0c9bc519c6363fb
montelucci <montelucci@gmail.com>
2020-01-07 06:46:03 +0100

    remove kanpen stuff, add dead end test

--- src/variety/araf.js
+++ src/variety/araf.js
@@ -60,7 +60,6 @@ Graphic:{
 		this.drawBGCells();
 		this.drawDashedGrid();
 
-		// this.drawQuesNumbers();
 		this.drawCircledNumbers();
 
 		this.drawBorders();
@@ -80,107 +79,8 @@ Encode:{
 	encodePzpr : function(type){
 		this.encodeNumber16();
 	},
-
-	decodeKanpen : function(){
-		this.fio.decodeCellQnum_kanpen();
-	},
-	encodeKanpen : function(){
-		this.fio.encodeCellQnum_kanpen();
-	}
 },
-//---------------------------------------------------------
-FileIO:{
-	decodeData : function(){
-		this.decodeCellQnum();
-		this.decodeBorderAns();
-	},
-	encodeData : function(){
-		this.encodeCellQnum();
-		this.encodeBorderAns();
-	},
 
-	kanpenOpen : function(){
-		this.decodeCellQnum_kanpen();
-		this.decodeAnsSquareRoom();
-	},
-	kanpenSave : function(){
-		this.encodeCellQnum_kanpen();
-		this.encodeAnsSquareRoom();
-	},
-
-	decodeAnsSquareRoom : function(){
-		var bd = this.board, rdata = [], line;
-		for(var i=0,rows=+this.readLine();i<rows;i++){
-			if(!(line=this.readLine())){ break;}
-			var pce = line.split(" ");
-			for(var n=0;n<4;n++){ if(!isNaN(pce[n])){ pce[n]=2*(+pce[n])+1;} }
-			bd.cellinside(pce[1],pce[0],pce[3],pce[2]).each(function(cell){ rdata[cell.id] = i;});
-		}
-		this.rdata2Border(false, rdata);
-		bd.roommgr.rebuild();
-	},
-	encodeAnsSquareRoom : function(){
-		var bd = this.board;
-		bd.roommgr.rebuild();
-		var rooms = bd.roommgr.components;
-		this.writeLine(rooms.length);
-		for(var id=0;id<rooms.length;id++){
-			var d = rooms[id].clist.getRectSize();
-			this.writeLine([(d.y1>>1), (d.x1>>1), (d.y2>>1), (d.x2>>1), ''].join(' '));
-		}
-	},
-
-	kanpenOpenXML : function(){
-		this.decodeCellQnum_shikaku_XMLBoard();
-		this.decodeAnsSquareRoom_XMLAnswer();
-	},
-	kanpenSaveXML : function(){
-		this.encodeCellQnum_shikaku_XMLBoard();
-		this.encodeAnsSquareRoom_XMLAnswer();
-	},
-
-	decodeCellQnum_shikaku_XMLBoard : function(){
-		this.decodeCellXMLBoard(function(cell, val){
-			if     (val>=  1){ cell.qnum = val;}
-			else if(val===-1){ cell.qnum = -2;}
-		});
-	},
-	encodeCellQnum_shikaku_XMLBoard : function(){
-		this.encodeCellXMLBoard(function(cell){
-			var val = 0;
-			if     (cell.qnum>=  1){ val = cell.qnum;}
-			else if(cell.qnum===-2){ val = -1;}
-			return val;
-		});
-	},
-
-	decodeAnsSquareRoom_XMLAnswer : function(){
-		var nodes = this.xmldoc.querySelectorAll('answer area');
-		var bd = this.board, rdata = [];
-		for(var i=0;i<nodes.length;i++){
-			var node = nodes[i];
-			var bx1 = 2*(+node.getAttribute('c0'))-1;
-			var by1 = 2*(+node.getAttribute('r0'))-1;
-			var bx2 = 2*(+node.getAttribute('c1'))-1;
-			var by2 = 2*(+node.getAttribute('r1'))-1;
-			for(var bx=bx1;bx<=bx2;bx+=2){ for(var by=by1;by<=by2;by+=2){
-				rdata[bd.getc(bx,by).id] = i;
-			}}
-		}
-		this.rdata2Border(false, rdata);
-		bd.roommgr.rebuild();
-	},
-	encodeAnsSquareRoom_XMLAnswer : function(){
-		var boardnode = this.xmldoc.querySelector('answer');
-		var bd = this.board;
-		bd.roommgr.rebuild();
-		var rooms = bd.roommgr.components;
-		for(var id=0;id<rooms.length;id++){
-			var d = rooms[id].clist.getRectSize();
-			boardnode.appendChild(this.createXMLNode('area',{r0:(d.y1>>1)+1,c0:(d.x1>>1)+1,r1:(d.y2>>1)+1,c1:(d.x2>>1)+1}));
-		}
-	}
-},
 //---------------------------------------------------------
 // 正解判定処理実行部
 AnsCheck:{
