84d0bf0f95d5480cfd0a0c3b27c9b66a10313f43
Robert Vollmert <rob@vllmrt.net>
2020-01-30 19:50:26 +0100

    herugolf: failing test for ansclear/undo (#126)

---
+++ test/variety/herugolf_test.js
@@ -0,0 +1,29 @@
+var assert = require("assert");
+
+var pzpr = require("../../");
+
+var puzzle = new pzpr.Puzzle();
+
+describe("Variety:herugolf", function() {
+	it("allows undoing through answer reset, #126", function() {
+		puzzle.open("herugolf/3/3/002p");
+
+		puzzle.setMode("play");
+		puzzle.mouse.inputPath(1, 1, 5, 1, 5, 3);
+
+		var getLine = function(border) {
+			var s = "";
+			for (var i = 0; i < border.length; i++) {
+				s = s + border[i].line;
+			}
+			return s;
+		};
+
+		assert.equal("110000001000", getLine(puzzle.board.border));
+
+		puzzle.ansclear();
+		puzzle.undo();
+
+		assert.equal("110000001000", getLine(puzzle.board.border));
+	});
+});
