21064dddf7eb396c2d78ec2021e41bc1527f7a19
Robert Vollmert <rob@vllmrt.net>
2020-02-12 20:14:07 +0100

    cbblock: add test case / rules example
    due to @tckmn

--- test/script/cbblock.js
+++ test/script/cbblock.js
@@ -8,6 +8,10 @@ ui.debug.addDebugData("cbblock", {
 			"pzprv3/cbblock/4/4/1 0 2 /0 2 0 /2 2 2 /0 2 2 /1 2 2 0 /0 2 2 2 /2 0 2 0 /"
 		],
 		[
+			"bkSubGt2",
+			"pzprv3/cbblock/4/4/2 0 1 /0 1 0 /1 1 2 /0 2 1 /2 2 1 0 /0 1 2 2 /1 0 1 0 /"
+		],
+		[
 			"bdDeadEnd",
 			"pzprv3/cbblock/4/4/1 0 2 /0 2 0 /2 2 2 /0 2 2 /2 2 2 0 /0 2 2 2 /2 0 2 0 /"
 		],
