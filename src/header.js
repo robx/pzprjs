d3f688a51e8de807be6b6800e834f9105db758d8
Harmen <alicebob@lijzij.de>
2020-04-18 11:01:49 +0200

    run prettier

--- src/header.js
+++ src/header.js
@@ -2,13 +2,13 @@ var pzpr = {
 	version: "<%= git.hash %>"
 };
 export default pzpr;
- 
-import Candle from 'pzpr-canvas';
+
+import Candle from "pzpr-canvas";
 pzpr.Candle = Candle;
 
 //eslint-disable-next-line no-unused-vars
-var document      = this.document      || pzpr.Candle.document;
+var document = this.document || pzpr.Candle.document;
 //eslint-disable-next-line no-unused-vars
-var DOMParser     = this.DOMParser     || pzpr.Candle.DOMParser;
+var DOMParser = this.DOMParser || pzpr.Candle.DOMParser;
 //eslint-disable-next-line no-unused-vars
 var XMLSerializer = this.XMLSerializer || pzpr.Candle.XMLSerializer;
