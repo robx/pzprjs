4edd63578b1c0f43fe56e36be48e7b562b517471
Lennard Sprong <x-sheep@users.noreply.github.com>
2020-11-08 11:58:35 +0100

    fillomino: Add test for ghost borders

--- test/variety/fillomino_test.js
+++ test/variety/fillomino_test.js
@@ -18,4 +18,32 @@ describe("Variety:fillomino", function() {
 		check = puzzle.check(true);
 		assert.equal(check.length, 1);
 	});
+
+	it("can add ghost borders", function() {
+		puzzle.open(
+			"pzprv3/fillomino/3/3/. . . /. 2 . /. . . /. . . /. . . /. . . /0 0 /0 0 /0 0 /0 0 0 /0 0 0 /"
+		);
+
+		var center = puzzle.board.getc(3, 3);
+
+		assert.equal(center.adjborder.top.qcmp, 0);
+
+		center.adjacent.left.setNum(2);
+		assert.equal(center.adjborder.top.qcmp, 1);
+
+		center.adjacent.right.setNum(2);
+		assert.equal(center.adjborder.top.qcmp, 0);
+
+		center.adjborder.left.setQans(1);
+		assert.equal(center.adjborder.top.qcmp, 1);
+
+		center.adjacent.right.setNum(-1);
+		assert.equal(center.adjborder.top.qcmp, 0);
+
+		center.adjborder.left.setQans(0);
+		assert.equal(center.adjborder.top.qcmp, 1);
+
+		center.adjacent.left.setNum(-1);
+		assert.equal(center.adjborder.top.qcmp, 0);
+	});
 });
