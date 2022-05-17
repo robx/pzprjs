59df8887796db47641d90ebf0e226e0d85601771
Lennard Sprong <x-sheep@users.noreply.github.com>
2022-05-17 17:20:35 +0200

    Fix drawRawCursor

--- src/variety-common/Graphic.js
+++ src/variety-common/Graphic.js
@@ -2023,7 +2023,7 @@ pzpr.classmgr.makeCommon({
 				h = w;
 			}
 
-			if (cursor.bankpiece !== null) {
+			if (this.board.bank && cursor.bankpiece !== null) {
 				var piece = this.board.bank.pieces[cursor.bankpiece];
 				if (cursor.bankpiece === this.board.bank.pieces.length) {
 					piece = this.board.bank.addButton;
