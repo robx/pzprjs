7b3f959531d356cd679e710ca855161cc9c022e0
Lennard Sprong <x-sheep@users.noreply.github.com>
2022-08-10 12:33:27 +0200

    Use thumbnail savefile for render test

--- test/puzzle/render_test.js
+++ test/puzzle/render_test.js
@@ -9,8 +9,23 @@ var testdata = require("../load_testdata.js");
 pzpr.variety.each(function(pid) {
 	describe(pid + " render test", function() {
 		it("render svg", function() {
+			var checks = testdata[pid].failcheck;
+			var path = pid + "/" + testdata[pid].url;
+
+			for (var i = checks.length - 1; i >= 0; i--) {
+				var testcase = checks[i];
+				if (testcase[0] !== null) {
+					continue;
+				}
+				if (testcase.length > 2 && testcase[2].skiprules) {
+					continue;
+				}
+				path = testcase[1];
+				break;
+			}
+
 			var puzzle = new pzpr.Puzzle();
-			puzzle.open(pid + "/" + testdata[pid].url, () => {
+			puzzle.open(path, () => {
 				puzzle.toBuffer("svg", 0, 30);
 			});
 		});
