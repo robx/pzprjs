a2377e0e443eca6dd5a371df7d30f887b0a44434
Lennard Sprong <x-sheep@users.noreply.github.com>
2022-11-21 18:37:03 +0100

    Add test coverage for irowake

--- test/variety/yajilin_test.js
+++ test/variety/yajilin_test.js
@@ -18,4 +18,22 @@ describe("Variety:yajilin", function() {
 		assert.equal(puzzle.board.cell[0].isCmp(), true);
 		assert.equal(puzzle.board.cell[3].isCmp(), true);
 	});
+	it("Check irowake adds different color", function() {
+		puzzle.open("yajilin/2/3/");
+		puzzle.setConfig("irowake", true);
+		puzzle.irowake();
+
+		puzzle.mouse.inputPath("left", 1, 1, 3, 1);
+		puzzle.mouse.inputPath("left", 1, 3, 3, 3);
+		puzzle.mouse.inputPath("left", 1, 5, 3, 5);
+
+		puzzle.irowake();
+
+		puzzle.setConfig("irowake", false);
+
+		var items = puzzle.board.linegraph.components;
+		assert.notEqual(items[0].color, items[1].color);
+		assert.notEqual(items[0].color, items[2].color);
+		assert.notEqual(items[1].color, items[2].color);
+	});
 });
