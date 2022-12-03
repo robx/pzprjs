b0cfce184809f9133021bf3c82de849367e2cdc7
Lennard Sprong <x-sheep@users.noreply.github.com>
2022-12-03 10:45:17 +0100

    Repair example

--- test/script/lollipops.js
+++ test/script/lollipops.js
@@ -1,11 +1,11 @@
 /* lollipops.js */
 
 ui.debug.addDebugData("lollipops", {
-	url: "5/5/3i1c1h3a",
+	url: "5/5/3m12g3a",
 	failcheck: [
 		[
 			null,
-			"pzprv3/lollipops/5/5/3 . . . . /. . . . . /1 . . . 1 /. . . . . /. . . 3 . /. 1 . . . /. . 1 . 2 /. . 2 . . /2 . . . . /. . 1 . . /"
+			"pzprv3/lollipops/5/5/3 . . . . /. . . . . /. . . . 1 /2 . . . . /. . . 3 . /. 1 . . . /. . 1 . 2 /1 . 2 . . /. . . . . /. . 1 . . /"
 		]
 	],
 	inputs: []
