ecf2c8a39ee22a9041d87d0e42e6e8e149783088
Lennard Sprong <x-sheep@users.noreply.github.com>
2022-12-03 10:28:50 +0100

    Add example puzzle

---
+++ test/script/lollipops.js
@@ -0,0 +1,12 @@
+/* lollipops.js */
+
+ui.debug.addDebugData("lollipops", {
+	url: "5/5/3i1c1h3a",
+	failcheck: [
+		[
+			null,
+			"pzprv3/lollipops/5/5/3 . . . . /. . . . . /1 . . . 1 /. . . . . /. . . 3 . /. 1 . . . /. . 1 . 2 /. . 2 . . /2 . . . . /. . 1 . . /"
+		]
+	],
+	inputs: []
+});
