b1146fe7a64dcb6bd495ef01ca6232f6b495d11d
Lennard Sprong <x-sheep@users.noreply.github.com>
2022-12-04 17:15:43 +0100

    Add more input tests

--- test/script/lollipops.js
+++ test/script/lollipops.js
@@ -51,6 +51,37 @@ ui.debug.addDebugData("lollipops", {
 			label: "Input aux dots with keyboard",
 			input: ["cursor,3,3", "key,4", "playmode", "cursor,3,3", "key,4"],
 			result: "pzprv3/lollipops/2/2/1 3 /2 . /. . /. + /"
+		},
+		{
+			label: "Default auto input",
+			input: [
+				"newboard,2,2",
+				"playmode",
+				"setconfig,mouseonly,false",
+				"cursor,1,1",
+				"mouse,leftx3,3,3"
+			],
+			result: "pzprv3/lollipops/2/2/. . /. . /. . /. 2 /"
+		},
+		{
+			label: "Drag aux dots",
+			input: ["mouse,right,3,1,1,1"],
+			result: "pzprv3/lollipops/2/2/. . /. . /+ + /. 2 /"
+		},
+		{
+			label: "Drag line",
+			input: ["mouse,left,1,2.1,1,3.9"],
+			result: "pzprv3/lollipops/2/2/. . /. . /+ + /2 2 /"
+		},
+		{
+			label: "Mouse-only input",
+			input: [
+				"newboard,2,2",
+				"setconfig,mouseonly,true",
+				"mouse,left,1,3",
+				"mouse,left,3,3"
+			],
+			result: "pzprv3/lollipops/2/2/. . /. . /. . /1 1 /"
 		}
 	]
 });
