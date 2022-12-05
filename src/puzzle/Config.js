e435fb036e7dbd8c034a5b97b1f3ee8c9d47735e
Lennard Sprong <x-sheep@users.noreply.github.com>
2022-12-05 17:53:12 +0100

    Add Lollipops

--- src/puzzle/Config.js
+++ src/puzzle/Config.js
@@ -45,6 +45,7 @@
 			this.add("context_marks", true);
 			this.add("dispqnumbg", false); /* yinyang: 問題のまるに背景色をつける */
 			this.add("undefcell", true); /* shugaku: 未確定マスはグレー表示にする */
+			this.add("mouseonly", false); /* lollipops: Alternative mouse input */
 
 			this.add("squarecell", true); /* セルは正方形にする */
 
@@ -372,6 +373,9 @@
 				case "dispqnumbg":
 					exec = pid === "yinyang";
 					break;
+				case "mouseonly":
+					exec = pid === "lollipops";
+					break;
 				case "undefcell":
 					exec = pid === "shugaku";
 					break;
@@ -455,6 +459,7 @@
 				case "disptype_yajilin":
 				case "disptype_interbd":
 				case "dispqnumbg":
+				case "mouseonly":
 					puzzle.redraw();
 					break;
 
