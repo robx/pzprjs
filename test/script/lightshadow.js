68e680f2aa205329c2ea79bea6db120f728e1a19
Lennard Sprong <x-sheep@users.noreply.github.com>
2022-12-12 20:21:06 +0100

    lightshadow: Always use question mark in example

--- test/script/lightshadow.js
+++ test/script/lightshadow.js
@@ -1,5 +1,5 @@
 ui.debug.addDebugData("lightshadow", {
-	url: "6/6/g54i6k7p-17k2i-149g",
+	url: "6/6/g54i6k7p1k2i-149g",
 	failcheck: [
 		[
 			"bkNumGe2",
@@ -27,7 +27,7 @@ ui.debug.addDebugData("lightshadow", {
 		],
 		[
 			null,
-			"pzprv3/lightshadow/6/6/. 1,2 0,2 . . . /0,3 . . . . . /1,3 . . . . . /. . . . . 1,11 /. . . . . 0,1 /. . . 0,10 1,4 . /1 0 0 2 1 1 /0 2 1 1 1 1 /0 2 1 1 1 1 /1 1 2 2 2 0 /2 2 2 1 1 0 /2 2 2 0 0 1 /"
+			"pzprv3/lightshadow/6/6/. 1,2 0,2 . . . /0,3 . . . . . /1,3 . . . . . /. . . . . 1,-2 /. . . . . 0,1 /. . . 0,10 1,4 . /1 0 0 2 1 1 /0 2 1 1 1 1 /0 2 1 1 1 1 /1 1 2 2 2 0 /2 2 2 1 1 0 /2 2 2 0 0 1 /"
 		]
 	],
 	inputs: [
