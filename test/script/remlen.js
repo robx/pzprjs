8d8624d5852ee5bbd0bbefda23fa90a7b20ea45b
Lennard Sprong <x-sheep@users.noreply.github.com>
2022-12-23 18:57:51 +0100

    Add example

---
+++ test/script/remlen.js
@@ -0,0 +1,12 @@
+/* remlen.js */
+
+ui.debug.addDebugData("remlen", {
+	url: "4/4/1qotl013i",
+	failcheck: [
+		[
+			null,
+			"pzprv3.2/remlen/4/4/0 0 0 /0 1 1 /1 0 1 /0 1 1 /1 1 1 0 /1 1 0 1 /0 1 0 0 /1 . . . /3 . . . /. . . . /. . . . /1l 1l 1l /0 1 0 /0 0 0 /1r 0 1 /1 0 0 1u /1 1 1d 1 /1d 1 1d 1u /"
+		]
+	],
+	inputs: []
+});
