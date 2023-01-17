be5f08f05224c33561696266091e57d182813d45
Lennard Sprong <x-sheep@users.noreply.github.com>
2023-01-17 19:26:03 +0100

    Add example

---
+++ test/script/news.js
@@ -0,0 +1,12 @@
+/* news.js */
+
+ui.debug.addDebugData("news", {
+	url: "4/4/2n8f9054a3826",
+	failcheck: [
+		[
+			null,
+			"pzprv3/news/4/4/4/0 0 0 0 /0 1 1 2 /1 1 3 2 /1 3 3 2 /. 4 . . /. . . . /3 . . . /. 2 . . /. . . . /2 . 4 1 /. . 1 2 /. . . . /"
+		]
+	],
+	inputs: []
+});
