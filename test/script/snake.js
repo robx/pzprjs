416908b349a7e2427cef2ec4d579b9dbca71dd8c
Lennard Sprong <x-sheep@users.noreply.github.com>
2023-01-25 10:54:57 +0100

    snake: Add manual number qcmp

--- test/script/snake.js
+++ test/script/snake.js
@@ -43,5 +43,19 @@ ui.debug.addDebugData("snake", {
 			null,
 			"pzprv3/snake/4/4/. . 1 . . /4 4 4 4 4 /. 6 0 0 4 /. 0 0 5 4 /. 0 0 4 0 /"
 		]
+	],
+	inputs: [
+		{
+			input: ["newboard,2,2", "editmode,number", "mouse,rightx2,3,-1"],
+			result: "pzprv3/snake/2/2/. . 2 /. 0 0 /. 0 0 /"
+		},
+		{
+			input: ["playmode", "mouse,left,3,-1", "mouse,right,3,3"],
+			result: "pzprv3/snake/2/2/. . c2 /. 0 0 /. 0 8 /"
+		},
+		{
+			input: ["editmode,auto", "cursor,-1,1", "key,1,right,1"],
+			result: "pzprv3/snake/2/2/. . c2 /1 1 0 /. 0 8 /"
+		}
 	]
 });
