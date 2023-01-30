df715500e1a399a7ae39efae584f0ff51f2306a5
Lennard Sprong <x-sheep@users.noreply.github.com>
2023-01-30 22:37:29 +0100

    Add example puzzle

---
+++ test/script/trainstations.js
@@ -0,0 +1,12 @@
+/* trainstations.js */
+
+ui.debug.addDebugData("trainstations", {
+	url: "6/6/g3n0m1j4h02n",
+	failcheck: [
+		[
+			null,
+			"pzprv3/trainstations/6/6/. 3 . . . . /. . . . 0 . /. . . . . . /1 . . . . 4 /. . 0 2 . . /. . . . . . /1 1 1r 0 1 /1 1 0 1 1 /1 0 1 0 1 /0 1 0 1l 0 /0 1 1 1 0 /1 0 1 1 1l /1 0 0 1 1 1 /0 0 1 0 1 0 /1 1 0 1 0 1 /1 0 1 0 1 1 /1d 1 1 0 0 1 /"
+		]
+	],
+	inputs: []
+});
