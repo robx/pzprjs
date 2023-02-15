632058974ddaa506cfe1ddbe261f5da9663165d9
Lennard Sprong <x-sheep@users.noreply.github.com>
2023-02-15 12:41:01 +0100

    arukone: Change example puzzle

--- test/script/arukone.js
+++ test/script/arukone.js
@@ -1,51 +1,56 @@
 /* arukone.js */
 
 ui.debug.addDebugData("arukone", {
-	url: "5/5/1m2h3g3h1m2",
+	url: "4/4/12l3123j",
 	failcheck: [
 		[
 			"brNoLine",
-			"pzprv3/arukone/5/5/allowempty/A . . . . /. . . B . /. C . C . /. A . . . /. . . . B /0 0 0 0 /0 0 0 0 /0 0 0 0 /0 0 0 0 /0 0 0 0 /0 0 0 0 0 /0 0 0 0 0 /0 0 0 0 0 /0 0 0 0 0 /"
+			"pzprv3/arukone/4/4/passallcell/A B . . /. . . . /C A B C /. . . . /0 0 0 /0 0 0 /0 0 0 /0 0 0 /0 0 0 0 /0 0 0 0 /0 0 0 0 /"
 		],
 		[
 			"lnBranch",
-			"pzprv3/arukone/5/5/allowempty/A . . . . /. . . B . /. C . C . /. A . . . /. . . . B /1 1 0 0 /0 0 0 0 /0 0 0 0 /0 0 0 0 /0 0 0 0 /0 1 0 0 0 /0 0 0 0 0 /0 0 0 0 0 /0 0 0 0 0 /"
+			"pzprv3/arukone/4/4/passallcell/A B . . /. . . . /C A B C /. . . . /0 1 0 /0 0 1 /0 0 0 /0 0 0 /0 0 1 0 /0 0 1 0 /0 0 0 0 /"
 		],
 		[
 			"lnCross",
-			"pzprv3/arukone/5/5/allowempty/A . . . . /. . . B . /. C . C . /. A . . . /. . . . B /1 1 0 0 /0 0 0 0 /0 1 1 0 /0 1 0 0 /0 0 0 0 /0 0 1 0 0 /0 0 1 0 0 /0 0 1 0 0 /0 0 0 0 0 /"
+			"pzprv3/arukone/4/4/passallcell/A B . . /. . . . /C A B C /. . . . /0 1 0 /1 1 1 /0 0 0 /0 0 0 /0 0 1 0 /1 0 1 1 /0 0 0 0 /"
 		],
 		[
 			"lcTripleNum",
-			"pzprv3/arukone/5/5/allowempty/A . . . . /. . . B . /. C . C . /. A . . . /. . . . B /1 1 0 0 /0 0 0 0 /0 1 0 0 /0 0 0 0 /0 0 0 0 /0 0 1 0 0 /0 0 1 0 0 /0 1 0 0 0 /0 0 0 0 0 /"
+			"pzprv3/arukone/4/4/passallcell/A B . . /. . . . /C A B C /. . . . /1 1 0 /0 1 0 /0 0 0 /0 0 0 /0 0 1 0 /0 1 0 0 /0 0 0 0 /"
 		],
 		[
 			"nmConnDiff",
-			"pzprv3/arukone/5/5/allowempty/A . . . . /. . . B . /. C . C . /. A . . . /. . . . B /1 1 0 0 /0 0 0 0 /0 1 0 0 /0 0 0 0 /0 0 0 0 /0 0 1 0 0 /0 0 1 0 0 /0 0 0 0 0 /0 0 0 0 0 /"
+			"pzprv3/arukone/4/4/passallcell/A B . . /. . . . /C A B C /. . . . /0 0 0 /1 1 0 /0 0 0 /0 0 0 /0 0 0 0 /1 0 1 0 /0 0 0 0 /"
 		],
 		[
 			"lcOnNum",
-			"pzprv3/arukone/5/5/allowempty/A . . . . /. . . B . /. C . C . /. A . . . /. . . . B /1 1 0 0 /0 0 0 0 /0 0 0 0 /1 1 0 0 /1 1 1 0 /0 0 1 0 0 /0 0 1 0 0 /0 0 1 0 0 /1 0 0 0 0 /"
+			"pzprv3/arukone/4/4/passallcell/A B . . /. . . . /C A B C /. . . . /0 0 0 /1 0 0 /0 0 0 /0 1 0 /1 0 0 0 /0 1 0 0 /0 1 0 0 /"
 		],
 		[
 			"lnDeadEnd",
-			"pzprv3/arukone/5/5/allowempty/A . . . . /. . . B . /. C . C . /. A . . . /. . . . B /1 1 0 0 /0 0 0 0 /0 0 0 0 /0 0 0 0 /0 0 0 0 /0 0 1 0 0 /0 0 1 0 0 /0 0 1 0 0 /0 0 0 0 0 /"
+			"pzprv3/arukone/4/4/passallcell/A B . . /. . . . /C A B C /. . . . /0 0 1 /0 1 0 /0 0 0 /0 0 0 /0 0 1 1 /0 0 0 1 /0 0 0 0 /"
 		],
 		[
 			"lcIsolate",
-			"pzprv3/arukone/5/5/allowempty/A . . . . /. . . B . /. C . C . /. A . . . /. . . . B /0 1 0 0 /1 0 0 0 /0 0 0 0 /0 0 0 0 /1 1 0 0 /0 1 1 0 0 /1 0 1 0 0 /1 0 1 0 0 /1 0 1 0 0 /"
+			"pzprv3/arukone/4/4/passallcell/A B . . /. . . . /C A B C /. . . . /0 0 1 /0 0 1 /0 0 0 /0 0 0 /0 0 1 1 /0 0 0 0 /0 0 0 0 /"
 		],
 		[
 			"nmNoLine",
-			"pzprv3/arukone/5/5/allowempty/A . . . . /. . . B . /. C . C . /. A . . . /. . . . B /1 1 1 1 /0 0 0 0 /0 0 0 0 /0 1 1 1 /0 0 0 0 /0 0 0 0 1 /0 0 0 0 1 /0 0 0 0 1 /0 0 0 0 0 /"
+			"pzprv3/arukone/4/4/passallcell/A B . . /. . . . /C A B C /. . . . /0 0 0 /1 0 0 /0 0 0 /0 0 0 /1 0 0 0 /0 1 0 0 /0 0 0 0 /"
 		],
 		[
 			"ceNoLine",
-			"pzprv3/arukone/5/5/passallcell/A . . . . /. . . B . /. C . C . /. A . . . /. . . . B /0 0 0 0 /0 0 0 1 /0 1 1 0 /1 0 0 0 /0 0 0 0 /1 0 0 0 0 /1 0 0 0 1 /1 0 0 0 1 /0 0 0 0 1 /"
+			"pzprv3/arukone/4/4/passallcell/A B . . /. . . . /C A B C /. . . . /0 1 0 /1 0 0 /0 0 0 /1 1 1 /1 0 1 0 /0 1 1 0 /1 0 0 1 /"
 		],
 		[
 			null,
-			"pzprv3/arukone/5/5/passallcell/A . . . . /. . . B . /. C . C . /. A . . . /. . . . B /1 1 1 1 /1 1 1 0 /-1 1 1 0 /-1 1 1 1 /1 1 1 1 /0 0 0 0 1 /1 -1 0 0 1 /1 -1 0 0 1 /1 -1 0 0 0 /"
+			"pzprv3/arukone/4/4/dontpassallcell/A B . . /. . . . /C A B C /. . . . /0 1 0 /1 0 0 /0 0 0 /1 1 1 /1 0 1 0 /0 1 1 0 /1 0 0 1 /",
+			{ skiprules: true }
+		],
+		[
+			null,
+			"pzprv3/arukone/4/4/passallcell/A B . . /. . . . /C A B C /. . . . /0 1 1 /1 0 1 /0 0 0 /1 1 1 /1 0 0 1 /0 1 1 0 /1 0 0 1 /"
 		]
 	],
 	inputs: [
