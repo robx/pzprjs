1a306eb966140b2bb312a144df5e084bd086448f
Lennard Sprong <x-sheep@users.noreply.github.com>
2023-02-25 21:14:58 +0100

    trainstations: Allow using question marks

--- test/script/trainstations.js
+++ test/script/trainstations.js
@@ -41,6 +41,21 @@ ui.debug.addDebugData("trainstations", {
 			{ skiprules: true }
 		],
 		[
+			"nmNotConseqFull",
+			"pzprv3/trainstations/4/4/# . 1 . /. 0 - . /. 0 2 . /# . - . /0 1 1 /1 1 1 /1 1 1 /0 1 1 /0 1 0 1 /1 1 0 0 /0 1 0 1 /",
+			{ skiprules: true }
+		],
+		[
+			null,
+			"pzprv3/trainstations/4/4/# . 4 . /. 0 - . /. 0 2 . /# . - . /0 1 1 /1 1 1 /1 1 1 /0 1 1 /0 1 0 1 /1 1 0 0 /0 1 0 1 /",
+			{ skiprules: true }
+		],
+		[
+			"nmRange",
+			"pzprv3/trainstations/4/4/. 2 . . /. . . . /. . . . /. 3 . . /1 1 1 /0 1 1 /0 1 1 /1 1 1 /1 0 0 1 /1 1 0 0 /1 0 0 1 /",
+			{ skiprules: true }
+		],
+		[
 			null,
 			"pzprv3/trainstations/6/6/. 3 . . . . /. . . . 0 . /. . . . . . /1 . . . . 4 /. . 0 2 . . /. . . . . . /1 1 1r 0 1 /1 1 0 1 1 /1 0 1 0 1 /0 1 0 1l 0 /0 1 1 1 0 /1 0 1 1 1l /1 0 0 1 1 1 /0 0 1 0 1 0 /1 1 0 1 0 1 /1 0 1 0 1 1 /1d 1 1 0 0 1 /"
 		]
