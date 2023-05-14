221cd801f48137ebd0b3baafce841ed1094b7d35
Lennard Sprong <x-sheep@users.noreply.github.com>
2023-05-14 10:14:31 +0200

    Add example puzzle

---
+++ test/script/magnets.js
@@ -0,0 +1,12 @@
+/* magnets.js */
+
+ui.debug.addDebugData("magnets", {
+	url: "5/5/g222j2h2g22g0g32fubmo3or00000000000400",
+	failcheck: [
+		[
+			null,
+			"pzprv3/magnets/5/5/0 1 1 1 /1 1 1 1 /1 0 0 1 /0 1 1 1 /0 1 1 0 /1 1 0 0 0 /0 0 0 1 1 /1 1 0 0 0 /1 1 0 1 1 /. . 2 2 . . . /. . . 2 . . 2 /2 . a1 a2 a1 a2 s2 /2 . a2 a1 a2 a1 s2 /. 2 a1 a2 # s2 a2 /. 0 s2 s2 a1 s2 a1 /2 3 a2 a1 a2 a1 a2 /"
+		]
+	],
+	inputs: []
+});
