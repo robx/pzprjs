71b812ffae0f85b0e66434ad33906dffec604763
Lennard Sprong <x-sheep@users.noreply.github.com>
2023-05-14 17:13:21 +0200

    Add example puzzle

---
+++ test/script/oyakodori.js
@@ -0,0 +1,12 @@
+/* oyakodori.js */
+
+ui.debug.addDebugData("oyakodori", {
+	url: "4/4/2i0d080890096060",
+	failcheck: [
+		[
+			null,
+			"pzprv3/oyakodori/4/4/. . 9 9 /4 4 3 . /. . . . /9 9 . 3 /0 0 0 /1 0 1 /0 0 1 /0 0 0 /0 1 1 0 /1 0 0 0 /0 0 0 1 /1 1 0 /0 0 1 /1 0 1 /0 1 0 /1 0 0 1 /0 1 0 0 /1 0 1 1 /"
+		]
+	],
+	inputs: []
+});
