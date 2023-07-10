4dd47cbb942680939aabffce3f8612d9befaa1e1
Lennard Sprong <x-sheep@users.noreply.github.com>
2023-07-10 10:58:09 +0200

    Add letter test

---
+++ test/script/tetrominous.js
@@ -0,0 +1,12 @@
+/* tetrominous.js */
+
+ui.debug.addDebugData("tetrominous", {
+	url: "5/5/c1ich3i2h4i0gcccg",
+	failcheck: [
+		[
+			null,
+			"pzprv3/tetrominous/5/5/* 1 . . . /* . . 3 . /. . 2 . . /4 . . . 0 /. * * * . /. . . . . /. . . . . /. . . . . /. . . . . /. . . . . /0 0 1 0 /0 1 0 1 /1 1 0 1 /0 1 0 1 /0 0 0 0 /0 0 1 0 1 /0 0 1 1 0 /0 1 0 0 0 /0 0 0 0 0 /"
+		]
+	],
+	inputs: []
+});
