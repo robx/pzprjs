6673aa62ae3220a4aeb944b7d80e0276a296c455
Lennard Sprong <x-sheep@users.noreply.github.com>
2023-08-12 10:27:30 +0200

    Add input tests

--- test/script/mannequin.js
+++ test/script/mannequin.js
@@ -35,5 +35,34 @@ ui.debug.addDebugData("mannequin", {
 			"pzprv3/mannequin/5/5/0 0 1 0 /1 1 1 0 /0 0 0 0 /0 0 1 0 /0 0 1 0 /0 1 0 0 0 /1 0 1 1 1 /1 1 1 1 1 /0 0 0 0 0 /3 . . 1 . /. . . . . /. . . . . /. . . 0 . /. . . . . /. . . . # /# . # # . /# . . # . /. . . . . /# . # # # /"
 		]
 	],
-	inputs: []
+	inputs: [
+		{
+			input: [
+				"newboard,3,2",
+				"editmode",
+				"key,0",
+				"playmode",
+				"mouse,left,1,1,1,3"
+			],
+			result:
+				"pzprv3/mannequin/2/3/0 0 /0 0 /0 0 0 /0 . . /. . . /# . . /# . . /"
+		},
+		{
+			result: function(puzzle, assert) {
+				var room = puzzle.board.getc(1, 1).room;
+
+				puzzle.check();
+				assert.equal(room.distance, 1);
+				assert(room.cmp);
+			}
+		},
+		{
+			input: ["editmode", "mouse,left,2,0,2,2"],
+			result: function(puzzle, assert) {
+				var room = puzzle.board.getc(1, 1).room;
+				assert.equal(room.distance, null);
+				assert(room.clist.checkCmp());
+			}
+		}
+	]
 });
