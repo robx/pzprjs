e5be972dc27254fe5bfce306b7c98610a73cda3d
Lennard Sprong <x-sheep@users.noreply.github.com>
2023-10-18 12:47:14 +0200

    Fix key input of dot

--- src/variety/hakoiri.js
+++ src/variety/hakoiri.js
@@ -176,7 +176,7 @@
 			} else if (ca === "3" || ca === "e" || ca === "d" || ca === "c") {
 				ca = "3";
 			} else if (ca === "4" || ca === "r" || ca === "f" || ca === "v") {
-				ca = this.pid !== "hakoiri" ? " " : "s1";
+				ca = this.pid === "tontonbeya" ? " " : "s1";
 			} else if (ca === "5" || ca === "t" || ca === "g" || ca === "b") {
 				ca = " ";
 			}
