b4c4ce3266f8ba45f7b4b2a015879a1b9b5358a2
Lennard Sprong <x-sheep@users.noreply.github.com>
2023-11-13 12:57:18 +0100

    yosenabe: Hide setter-specific errors

--- test/script/yosenabe.js
+++ test/script/yosenabe.js
@@ -3,7 +3,7 @@
 ui.debug.addDebugData("yosenabe", {
 	url: "5/5/d1hgm1i3j2i5k5ki2o1l2k3",
 	failcheck: [
-		["brNoLine", "pzprv3/yosenabe/5/5"],
+		["brNoLine", "pzprv3/yosenabe/5/5", { skiprules: true }],
 		[
 			"lnBranch",
 			"pzprv3/yosenabe/5/5/o1 i3 i o2 i5 /. . o5 . i /i2 . . . i /i . o1 . . /i o2 i i o3 /0 0 0 0 /1 0 0 0 /0 0 0 0 /0 0 0 0 /0 0 0 0 /1 0 0 0 0 /1 0 0 0 0 /0 0 0 0 0 /0 0 0 0 0 /"
@@ -26,11 +26,13 @@ ui.debug.addDebugData("yosenabe", {
 		],
 		[
 			"bnIllegalPos",
-			"pzprv3/yosenabe/5/5/o1 i3 i o2 i5 /. . o5 . i /i2 4 . . i /i . o1 . . /i o2 i i o3 /1 0 1 -1 /0 -1 1 1 /0 0 0 -1 /0 0 0 0 /0 0 0 0 /0 0 -1 -1 -1 /0 0 -1 -1 -1 /0 0 0 0 -1 /0 0 0 0 0 /"
+			"pzprv3/yosenabe/5/5/o1 i3 i o2 i5 /. . o5 . i /i2 4 . . i /i . o1 . . /i o2 i i o3 /1 0 1 -1 /0 -1 1 1 /0 0 0 -1 /0 0 0 0 /0 0 0 0 /0 0 -1 -1 -1 /0 0 -1 -1 -1 /0 0 0 0 -1 /0 0 0 0 0 /",
+			{ skiprules: true }
 		],
 		[
 			"bkDoubleBn",
-			"pzprv3.1/yosenabe/5/5/o1 i3 i o2 i5 /. . o5 . i /i2 . . . i /i2 . o1 . . /i2 o2 i i o3 /0 0 0 0 /0 0 0 0 /0 0 0 0 /0 0 0 0 /1 0 0 0 /0 0 0 0 0 /0 0 0 0 0 /0 0 0 0 0 /0 0 0 0 0 /0 0 0 0 0 /0 0 0 0 0 /0 0 0 0 0 /0 0 0 0 0 /0 0 0 0 0 /"
+			"pzprv3.1/yosenabe/5/5/o1 i3 i o2 i5 /. . o5 . i /i2 . . . i /i2 . o1 . . /i2 o2 i i o3 /0 0 0 0 /0 0 0 0 /0 0 0 0 /0 0 0 0 /1 0 0 0 /0 0 0 0 0 /0 0 0 0 0 /0 0 0 0 0 /0 0 0 0 0 /0 0 0 0 0 /0 0 0 0 0 /0 0 0 0 0 /0 0 0 0 0 /0 0 0 0 0 /",
+			{ skiprules: true }
 		],
 		[
 			"bkSumNeBn",
