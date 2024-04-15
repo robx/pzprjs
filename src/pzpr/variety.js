3260a2e44c190a48232b5e989f19836875ad6fe8
Lennard Sprong <x-sheep@users.noreply.github.com>
2024-04-15 10:15:38 +0200

    Add LIX Loop

--- src/pzpr/variety.js
+++ src/pzpr/variety.js
@@ -244,6 +244,7 @@
 			lineofsight: [0, 0, "サイトライン", "Line of Sight"],
 			lither: [0, 0, "Litherslink", "Litherslink"],
 			lits: [1, 1, "ＬＩＴＳ", "LITS", "lits"],
+			lixloop: [0, 0, "LIX Loop", "LIX Loop", "yajilin"],
 			lohkous: [0, 0, "Lohkous", "Lohkous"],
 			lollipops: [0, 0, "ペロペロキャンディ", "Lollipops"],
 			lookair: [0, 0, "るっくえあ", "Look-Air"],
