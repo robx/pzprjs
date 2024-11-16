30c5667988c3e8e7e2e439221a9acab51b21d45c
Lennard Sprong <x-sheep@users.noreply.github.com>
2024-11-16 09:59:23 +0100

    Add example

--- test/script/archipelago.js
+++ test/script/archipelago.js
@@ -1,8 +1,29 @@
 /* archipelago.js */
 
 ui.debug.addDebugData("archipelago", {
-	url: "3/2/1j./",
-	failcheck: [[null, "pzprv3/archipelago/2/3/1 . . /. # - /"]],
+	url: "6/6/2o.g3h.n1p1",
+	failcheck: [
+		[
+			"bkSizeNe",
+			"pzprv3/archipelago/6/6/2 . # # . . /. # . . - . /3 # . - # # /. . . . . . /1 . . . . . /. . . . . 1 /"
+		],
+		[
+			"bsNoSequence",
+			"pzprv3/archipelago/6/6/2 # . . # . /. . # # - . /3 # . - . . /. # . . . . /1 . . # # . /. . . . . 1 /"
+		],
+		[
+			"bsSameNum",
+			"pzprv3/archipelago/6/6/2 # . . . . /. . # # - . /3 # . - . . /. # . . . . /1 . . . . . /. # # # . 1 /"
+		],
+		[
+			"bkNoChain",
+			"pzprv3/archipelago/6/6/2 # . . . . /. . # # - . /3 # . - . . /. # . . . . /1 . . . . . /. . . . . 1 /"
+		],
+		[
+			null,
+			"pzprv3/archipelago/6/6/2 # + + + + /+ + # # - + /3 # + - + + /+ # + + + + /1 + + # # + /+ + + + + 1 /"
+		]
+	],
 	inputs: [
 		{
 			input: ["newboard,6,2", "cursor,5,3", "mouse,right,5,3"],
