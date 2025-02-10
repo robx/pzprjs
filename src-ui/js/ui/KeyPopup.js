fbb77483ed66b60cfea2e40969579d13ebe253ea
Lennard Sprong <x-sheep@users.noreply.github.com>
2025-02-10 08:00:20 +0100

    Add Orbital

--- src-ui/js/ui/KeyPopup.js
+++ src-ui/js/ui/KeyPopup.js
@@ -218,7 +218,8 @@ ui.keypopup = {
 		nibunnogo: [4, 0],
 		mintonette: [10, 0],
 		balloon: [10, 0],
-		tilecity: [10, 0]
+		tilecity: [10, 0],
+		orbital: [124, 0]
 	},
 
 	//---------------------------------------------------------------------------
@@ -880,6 +881,7 @@ ui.keypopup = {
 	},
 
 	generate_trainstations: function(mode) {
+		var orbital = ui.puzzle.pid === "orbital";
 		this.generate_main(
 			[
 				"0",
@@ -893,8 +895,8 @@ ui.keypopup = {
 				"8",
 				"9",
 				" ",
-				["-", "?"],
-				["q", "╋"]
+				["-", orbital ? "●" : "?"],
+				["q", orbital ? "○" : "╋"]
 			],
 			4
 		);
