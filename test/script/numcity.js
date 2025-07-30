ddd0f0c041d925a28122201bcef23a81ee58b051
Lennard Sprong <x-sheep@users.noreply.github.com>
2025-07-30 21:41:07 +0200

    Add example

---
+++ test/script/numcity.js
@@ -0,0 +1,12 @@
+/* numcity.js */
+
+ui.debug.addDebugData("numcity", {
+	url: "4/4/j2i13l4i0s08",
+	failcheck: [
+		[
+			null,
+			"pzprv3/numcity/4/4/. . . . /2 . . . /1 3 . . /. . . . /1 1 1 2 /. 2 2 1 /. . 3 1 /1 1 1 4 /0 0 X /0 0 X /0 0 X /0 0 0 /X X X 0 /0 0 0 0 /0 0 0 X /"
+		]
+	],
+	inputs: []
+});
