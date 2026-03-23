dac95314ed423157d8d0806da24432cde3da79b4
mashinosatoshi <mashino.satoshi@gmail.com>
2026-03-24 01:22:57 +0900

    Fix Fillomino [?] region size validation (fixes #462)

--- test/variety/fillomino_test.js
+++ test/variety/fillomino_test.js
@@ -46,4 +46,21 @@ describe("Variety:fillomino", function() {
 		center.adjacent.left.setNum(-1);
 		assert.equal(center.adjborder.top.qcmp, 0);
 	});
+
+	it("detects same size regions with ? properly", function() {
+		var p = new pzpr.Puzzle();
+		p.open("fillomino/5/1");
+		p.board.getc(1,1).setNum(-2);
+		p.board.getc(3,1).setNum(-2);
+		p.board.getb(2,1).setQans(0);
+		p.board.getb(4,1).setQans(1);
+		p.board.getc(5,1).setNum(-2);
+		p.board.getc(7,1).setNum(-2);
+		p.board.getb(6,1).setQans(0);
+		p.board.getb(8,1).setQans(1);
+		p.board.getc(9,1).setNum(1);
+		p.board.numblkgraph.rebuild();
+		var check = p.check(true);
+		assert.equal(check[0], "bsSameNum");
+	});
 });
