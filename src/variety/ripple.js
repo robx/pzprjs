db7213925a26cd054b67ac8c94990f35e392befa
Lennard Sprong <x-sheep@users.noreply.github.com>
2026-03-27 09:04:55 +0100

    Add invalid cells

--- src/variety/ripple.js
+++ src/variety/ripple.js
@@ -12,7 +12,7 @@
 	// マウス入力系
 	MouseEvent: {
 		inputModes: {
-			edit: ["border", "number", "clear"],
+			edit: ["border", "empty", "number", "clear"],
 			play: ["number", "clear"]
 		},
 		autoedit_func: "areanum",
@@ -20,7 +20,7 @@
 	},
 	"MouseEvent@meander": {
 		inputModes: {
-			edit: ["border", "number", "clear"],
+			edit: ["border", "empty", "number", "clear"],
 			play: ["number", "dragnum+", "dragnum-", "clear"]
 		},
 		mouseinput: function() {
@@ -106,7 +106,18 @@
 	// キーボード入力系
 	KeyEvent: {
 		enablemake: true,
-		enableplay: true
+		enableplay: true,
+
+		keyinput: function(ca) {
+			if ((ca === "q" || ca === "w") && this.puzzle.editmode) {
+				var cell = this.cursor.getc();
+				if (!cell.isnull) {
+					cell.setValid(cell.ques !== 7 ? 7 : 0);
+				}
+			} else {
+				this.key_inputqnum(ca);
+			}
+		}
 	},
 
 	//---------------------------------------------------------
@@ -115,6 +126,13 @@
 		enableSubNumberArray: true,
 		maxnum: function() {
 			return this.room.clist.length;
+		}
+	},
+	Border: {
+		isQuesBorder: function() {
+			return (
+				this.ques || this.sidecell[0].isEmpty() || this.sidecell[1].isEmpty()
+			);
 		}
 	},
 	Board: {
@@ -139,6 +157,13 @@
 	Graphic: {
 		gridcolor_type: "DLIGHT",
 
+		getBGCellColor: function(cell) {
+			if (!cell.isValid()) {
+				return "black";
+			}
+			return this.getBGCellColor_error1(cell);
+		},
+
 		paint: function() {
 			this.drawBGCells();
 			this.drawTargetSubNumber();
@@ -153,6 +178,14 @@
 			this.drawChassis();
 
 			this.drawCursor();
+		},
+
+		getBorderColor: function(border) {
+			if (border.isQuesBorder()) {
+				return border.error === 1 ? this.errcolor1 : "black";
+			}
+
+			return this.getBorderColor_qans(border);
 		}
 	},
 
@@ -162,10 +195,12 @@
 		decodePzpr: function(type) {
 			this.decodeBorder();
 			this.decodeNumber16();
+			this.decodeEmpty();
 		},
 		encodePzpr: function(type) {
 			this.encodeBorder();
 			this.encodeNumber16();
+			this.encodeEmpty();
 		},
 
 		decodeKanpen: function() {
@@ -210,6 +245,31 @@
 			this.encodeAreaRoom_XMLBoard();
 			this.encodeCellQnum_XMLBoard();
 			this.encodeCellAnum_XMLAnswer();
+		},
+
+		decodeCellQnum: function() {
+			this.decodeCell(function(cell, ca) {
+				if (ca === "#") {
+					cell.ques = 7;
+				} else if (ca === "-") {
+					cell.qnum = -2;
+				} else if (ca !== ".") {
+					cell.qnum = +ca;
+				}
+			});
+		},
+		encodeCellQnum: function() {
+			this.encodeCell(function(cell) {
+				if (cell.ques === 7) {
+					return "# ";
+				} else if (cell.qnum >= 0) {
+					return cell.qnum + " ";
+				} else if (cell.qnum === -2) {
+					return "- ";
+				} else {
+					return ". ";
+				}
+			});
 		},
 
 		UNDECIDED_NUM_XML: 0
