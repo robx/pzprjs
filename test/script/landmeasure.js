5eabfce498eabd3a613e5d715010f6a761a80580
Lennard Sprong <x-sheep@users.noreply.github.com>
2026-05-26 19:40:30 +0200

    Add example

---
+++ test/script/landmeasure.js
@@ -0,0 +1,12 @@
+/* landmeasure.js */
+
+ui.debug.addDebugData("landmeasure", {
+	url: "6/6/n7h.g2u9i3o0h2h",
+	failcheck: [
+		[
+			null,
+			"pzprv3/landmeasure/6/6/. . . . . . . /. 7 . . - . 2 /. . . . . . . /. . . . . . . /. 9 . . . 3 . /. . . . . . . /. 0 . . 2 . . /. # # # . # /# . # . # # /# # # # . . /# . . # # # /. # # # . # /. . . # # . /"
+		]
+	],
+	inputs: []
+});
